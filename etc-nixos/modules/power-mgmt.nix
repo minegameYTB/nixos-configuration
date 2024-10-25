8f25ed0e77baca17368a60b69146167b9a350d2e
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-10-25 14:22:47 +0200

    Update power-mgmt.nix

--- etc-nixos/modules/power-mgmt.nix
+++ etc-nixos/modules/power-mgmt.nix
@@ -10,8 +10,8 @@
      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
  
     #Optional helps save long term battery health
-     START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-     STOP_CHARGE_THRESH_BAT0 = 90; # 80 and above it stops charging
+    #START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+    #STOP_CHARGE_THRESH_BAT0 = 90; # 80 and above it stops charging
    };
  }; 
 
