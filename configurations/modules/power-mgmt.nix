d52ab8aa1c45519840789b9b6167af5149ec62a0
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-10-31 10:58:57 +0100

    modules/power-mgmt.nix: fix syntax

--- configurations/modules/power-mgmt.nix
+++ configurations/modules/power-mgmt.nix
@@ -8,7 +8,7 @@
  services.tlp = {
    enable = true;
    settings = {
-     CPU_BOOST_ON_AC = 1
+     CPU_BOOST_ON_AC = 1;
      CPU_BOOST_ON_BAT = 0;
      CPU_SCALING_GOVERNOR_ON_AC = "performance"
      CPU_SCALING_GOVERNOR_ON_BAT = "powersave"
@@ -16,7 +16,7 @@
  };
 
  ### Disable Gnome power management
- services.power-profiles-daemon.enable = false
+ services.power-profiles-daemon.enable = false;
 
  ### Powertop
  powerManagement.powertop.enable = true;
