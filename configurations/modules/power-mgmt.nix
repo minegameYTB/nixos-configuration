796a4f656dcf554af97f271a749d7742150fc720
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-10-31 11:00:02 +0100

    modules/power-mgmt.nix: fix syntax on cpu governor part

--- configurations/modules/power-mgmt.nix
+++ configurations/modules/power-mgmt.nix
@@ -10,8 +10,8 @@
    settings = {
      CPU_BOOST_ON_AC = 1;
      CPU_BOOST_ON_BAT = 0;
-     CPU_SCALING_GOVERNOR_ON_AC = "performance"
-     CPU_SCALING_GOVERNOR_ON_BAT = "powersave"
+     CPU_SCALING_GOVERNOR_ON_AC = "performance";
+     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
    };
  };
 
