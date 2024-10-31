4e57fcc5f2e5d159d1999f9c2a29bf3f6b15b59e
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-10-31 03:04:11 +0100

    configuration.nix: remove modules/power-mgmt.nix entry

--- configurations/configuration.nix
+++ configurations/configuration.nix
@@ -14,10 +14,6 @@
       ./modules/system-pkgs.nix    ### System packages
       ./modules/systemd.nix        ### Systemd services
       ./modules/x11.nix            ### Related to x11 (Graphical server)
-
-     #./modules/power-mgmt.nix     ### Related to power management 
-                                   ### (for laptop, disable this if you don't need it)
-
       ./modules/vm-host.nix        ### To add qemu/kvm as an desktop hypervisor
      #./modules/vm-guest.nix       ### Optionnal (add support for qemu/kvm guest)
       ./modules/networking.nix     ### Related to network
