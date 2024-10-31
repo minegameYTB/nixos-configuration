a414f55241a516b2ea5422e672ce29e193cf5b70
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-10-31 15:43:35 +0100

    profiles/hp-probook-profile.nix: correct path

--- profiles/hp-probook-profile.nix
+++ profiles/hp-probook-profile.nix
@@ -6,7 +6,7 @@
    [ ../configurations/hardware-configuration/hp-probook.nix      ### Hardware configuration file (Include the results of the hardware scan.)
      ../configurations/modules/app-opts/hp-probook.nix            ### Programs with options
      ../configurations/modules/networking/hp-probook.nix          ### Related to network
-     ../configurations/modules/laptop/power-mgmt.nix              ### For laptop battery life
+     ../configurations/modules/specific/laptop/power-mgmt.nix              ### For laptop battery life
      ../configurations/modules/specific/desktop/gnome.nix         ### Related to GNOME DE
      ../configurations/modules/specific/desktop/x11.nix           ### Related to x11 Server (GUI server)
      ../configurations/modules/specific/vm/host/qemu-kvm-host.nix ### To add qemu/kvm as an desktop hypervisor
