858d1c5a8e83f7a7e32aece79364765893b46cfb
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-10-31 16:00:04 +0100

    add new modules to the configuration

--- profiles/hp-probook-profile.nix
+++ profiles/hp-probook-profile.nix
@@ -6,9 +6,11 @@
    [ ../configurations/hardware-configuration/hp-probook.nix      ### Hardware configuration file (Include the results of the hardware scan.)
      ../configurations/modules/app-opts/hp-probook.nix            ### Programs with options
      ../configurations/modules/networking/hp-probook.nix          ### Related to network
-     ../configurations/modules/specific/laptop/power-mgmt.nix              ### For laptop battery life
+     ../configurations/modules/specific/laptop/power-mgmt.nix     ### For laptop battery life
      ../configurations/modules/specific/desktop/gnome.nix         ### Related to GNOME DE
      ../configurations/modules/specific/desktop/x11.nix           ### Related to x11 Server (GUI server)
      ../configurations/modules/specific/vm/host/qemu-kvm-host.nix ### To add qemu/kvm as an desktop hypervisor
+     ../configurations/modules/specific/desktop/sound.nix         ### Sound server
+     ../configurations/modules/specific/desktop/printer.nix       ### CUPS server
    ];
 }
