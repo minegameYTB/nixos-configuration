4c9ab9d65948f3bdd3df5953af070b9c49b4daa5
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-03 21:01:31 +0100

    modules/.../specific/test-app: remove 'testing app'

--- configurations/modules/specific/test-apps/hp-probook.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
- ### This file will be used for testing things (on the long term ig) on my distribution (may be change anytime)
- ### ollama
- services.ollama.enable = true;
- 
-}
