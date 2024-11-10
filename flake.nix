5b15376824b77857e92d812e402d4165fa1e8d2c
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-10 15:52:52 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,58 +1,34 @@
-### (Flake created with https://librephoenix.com/2023-10-21-intro-flake-config-setup-for-new-nixos-users#org81dbd1d)
-
+### I used https://github.com/drupol/my-own-nixpkgs to create this monorepo
 {
-  description = "A flake with my configuration";
+  description = "monorepo for testing things";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nix-monorepo.url = "github:minegameYTB/nix-monorepo";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    # use the following for unstable:
-    # nixpkgs.url = "nixpkgs/nixos-unstable";
-
-    # or any branch you want:
-    # nixpkgs.url = "nixpkgs/{BRANCH-NAME}";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    pkgs-by-name-for-flake-parts.url = "github:drupol/pkgs-by-name-for-flake-parts";
   };
 
-  outputs = inputs@{ self, nixpkgs, nix-monorepo, home-manager, ... }: 
-  let
-    lib = nixpkgs.lib;
-    system = "x86_64-linux";
-  in {
-    nixosConfigurations = {
-      hp-probook = lib.nixosSystem {
-        system = system;
-        modules = [
-          ./configurations/configuration.nix
-          ./profiles/hp-probook-profile.nix
-          nix-monorepo.nixosModules.nix-monorepo
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.minegame = import ./configurations/home-manager/home.nix;
-          }
-        ];
+  outputs = inputs@{ self, nixpkgs, flake-parts, systems, ... }:
+    let
+      pkgs = import nixpkgs {};
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+        inputs.pkgs-by-name-for-flake-parts.flakeModule
+        ./imports/overlay.nix
+        ./imports/formatter.nix
+        ./imports/pkgs-all.nix
+      ];
+    
+      nixosModules = {
+        "nix-monorepo" = { config, pkgs, ... }: {
+           option = {};
+           config = {};
+         };
       };
     };
-    nixosConfigurations = {
-      hp-240 = lib.nixosSystem {
-        system = system;
-        modules = [
-          ./configurations/configuration.nix
-          ./profiles/hp-240-profile.nix
-           nix-monorepo.nixosModules.nix-monorepo
-           home-manager.nixosModules.home-manager
-           {
-             home-manager.useGlobalPkgs = true;
-             home-manager.useUserPackages = true;
-             home-manager.users.minegame = import ./configurations/home-manager/home.nix;
-           }
-        ];
-      };
-    };
-  };
 }
