c5ec3a808608498921ffcc17ae764787539f245c
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-10 15:17:27 +0100

    flake.nix: correct typo

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nix-monorepo.url = "github:minegameYTB/nix-monorepo"
+    nix-monorepo.url = "github:minegameYTB/nix-monorepo";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
