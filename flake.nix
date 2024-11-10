4c62f5292640e207b22b212b636d8e32e7683e3a
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-10 16:37:40 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   description = "monorepo for testing things";
 
   inputs = {
+<<<<<<< HEAD
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
@@ -31,4 +32,54 @@
          };
       };
     };
+=======
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # use the following for unstable:
+    # nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    # or any branch you want:
+    # nixpkgs.url = "nixpkgs/{BRANCH-NAME}";
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: 
+  let
+    lib = nixpkgs.lib;
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations = {
+      hp-probook = lib.nixosSystem {
+        system = system;
+        modules = [
+          ./configurations/configuration.nix
+          ./profiles/hp-probook-profile.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.minegame = import ./configurations/home-manager/home.nix;
+          }
+        ];
+      };
+    };
+    nixosConfigurations = {
+      hp-240 = lib.nixosSystem {
+        system = system;
+        modules = [
+          ./configurations/configuration.nix
+          ./profiles/hp-240-profile.nix
+           home-manager.nixosModules.home-manager
+           {
+             home-manager.useGlobalPkgs = true;
+             home-manager.useUserPackages = true;
+             home-manager.users.minegame = import ./configurations/home-manager/home.nix;
+           }
+        ];
+      };
+    };
+  };
+>>>>>>> parent of e44915f (flake.nix: Add custom monorepo from github)
 }
