b6c6ea44166f6d7c1355e2cf5fa3cdfbe3fe0412
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-12 15:54:54 +0100

    profile/hp-240.nix: add autologin option

--- profiles/hp-240-profile.nix
+++ profiles/hp-240-profile.nix
@@ -6,11 +6,12 @@
    [ ../configurations/hardware-configuration/hp-240.nix          ### Hardware configuration file (Include the results of the hardware scan.)
      ../configurations/modules/app-opts/hp-240.nix                ### Programs with options
      ../configurations/modules/networking/hp-240.nix              ### Related to network
-     #../configurations/modules/specific/laptop/power-mgmt.nix     ### For laptop battery life
+    #../configurations/modules/specific/laptop/power-mgmt.nix     ### For laptop battery life
      ../configurations/modules/specific/desktop/gnome.nix         ### Related to GNOME DE
      ../configurations/modules/specific/desktop/x11.nix           ### Related to x11 Server (GUI server)
     #../configurations/modules/specific/vm/host/qemu-kvm-host.nix ### To add qemu/kvm as an desktop hypervisor
      ../configurations/modules/specific/desktop/sound.nix         ### Sound server
      ../configurations/modules/specific/desktop/printer.nix       ### CUPS server
+     ../configurations/modules/specific/desktop/autologin.nix     ### Permit autologin
    ];
 }
