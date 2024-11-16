e0d32bf51bef41bf40607c066ec166bcbc73100d
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-16 15:36:49 +0100

    change tmp configuration

--- profiles/hp-240-profile.nix
+++ profiles/hp-240-profile.nix
@@ -6,6 +6,7 @@
    [ ../configurations/hardware-configuration/hp-240.nix          ### Hardware configuration file (Include the results of the hardware scan.)
      ../configurations/modules/app-opts/hp-240.nix                ### Programs with options
      ../configurations/modules/networking/hp-240.nix              ### Related to network
+     ../configurations/modules/system/tmp-on-disk.nix		  ### Use /tmp on disk
     #../configurations/modules/specific/laptop/power-mgmt.nix     ### For laptop battery life
      ../configurations/modules/specific/desktop/gnome.nix         ### Related to GNOME DE
      ../configurations/modules/specific/desktop/x11.nix           ### Related to x11 Server (GUI server)
--- profiles/vm-no-gui-profiles.nix
+++ profiles/vm-no-gui-profiles.nix
@@ -6,6 +6,7 @@
    [ ../configurations/hardware-configuration/vm-no-gui.nix         ### Hardware configuration file (Include the results of the hardware scan.)
      ../configurations/modules/app-opts/vm-no-gui.nix               ### Programs with options
      ../configurations/modules/networking/vm-no-gui.nix             ### Related to network
+     ../configurations/modules/system/tmp-on-disk.nix		    ### Use /tmp on disk
      ../configurations/modules/specific/vm/guest/qemu-kvm-guest.nix ### To use optimisation of qemu/kvm
    ];
 }
