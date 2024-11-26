e7ed128b0caca7331b0fd5567250956a4337ac1b
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-26 03:31:42 +0100

    overlays/coreutils.nix: change overlays to override

--- configurations/modules/overlays/coreutils.nix
+++ configurations/modules/overlays/coreutils.nix
@@ -1,5 +1,6 @@
 self: super: {
-  coreutils-full = super.coreutils-full.overrideAttrs (oldAttrs: {
-    configureFlags = oldAttrs.configureFlags or [] ++ [ "--disable-single-binary" ];
-  });
+  coreutils = super.coreutils.override {
+    singleBinary = "no";
+  };
 }
+
