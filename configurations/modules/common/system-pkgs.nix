56a83425640df6d03a41183a29e10fd4215d1cd2
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-26 04:21:28 +0100

    sync changes again

--- configurations/modules/common/system-pkgs.nix
+++ configurations/modules/common/system-pkgs.nix
@@ -5,14 +5,14 @@
  nixpkgs.config.allowUnfree = true;
 
  nixpkgs.overlays = [
-   (import ../overlays/coreutils.nix)
+   (import ../overlays/coreutils-full.nix) ### Overlays for coreutils-full (just compile tools for users and not runtime deps for software)
+  #(import ../overlays/package-name.nix)
  ];
 
  # List packages installed in system profile. To search, run:
  # $ nix search wget
  environment.systemPackages = with pkgs; [
   #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-   coreutils-full
    micro
    wget2
    efibootmgr
