3200232183d6c5fd61cdd5c09a4f26ae4d820a47
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-11-26 02:23:18 +0100

    sync change from flake branch to flake-nixos-branch

--- configurations/configuration.nix
+++ configurations/configuration.nix
@@ -5,24 +5,23 @@
 { config, pkgs, ... }:
 
 {
-  imports = 
-    [ ./modules/common/bootloader.nix     ### Bootloader configuration
-      ./modules/common/system-opts.nix    ### System options
-      ./modules/common/users.nix          ### User settings
-      ./modules/common/system-pkgs.nix    ### System packages
-     #./modules/common/systemd.nix        ### Systemd services
-      ./modules/common/timezone.nix       ### Timezone
-      ./modules/common/security.nix       ### Security
-    ];
+ imports = 
+   [ ./modules/common/bootloader.nix     ### Bootloader configuration
+     ./modules/common/system-opts.nix    ### System options
+     ./modules/common/users.nix          ### User settings
+     ./modules/common/system-pkgs.nix    ### System packages
+    #./modules/common/systemd.nix        ### Systemd services
+     ./modules/common/timezone.nix       ### Timezone
+     ./modules/common/security.nix       ### Security
+   ];
 
 ###----------------------------------------------------------------
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+ # This value determines the NixOS release from which the default
+ # settings for stateful data, like file locations and database versions
+ # on your system were taken. It‘s perfectly fine and recommended to leave
+ # this value at the release version of the first install of this system.
+ # Before changing this value read the documentation for this option
+ # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+ system.stateVersion = "24.05"; # Did you read the comment?
 }
