d93bdc5043a2860c8558d143fe739f7c51b6ea79
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-12-01 02:36:51 +0100

    modules/.../gnome.nix: readd gnome-console app

--- configurations/modules/specific/desktop/gnome.nix
+++ configurations/modules/specific/desktop/gnome.nix
@@ -18,7 +18,6 @@
    geary             ### Geary
    gnome-tour        ### Gnome Tour
    epiphany          ### Gnome Web
-   gnome-console     ### Gnome console
    yelp              ### Gnome help
    gnome-maps        ### Gnome maps
    gnome-connections ### Gnome connections
