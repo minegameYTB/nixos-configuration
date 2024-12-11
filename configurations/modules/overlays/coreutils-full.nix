cbf0167fa27a229f630d5451c4027ae6b72e6868
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-12-11 04:45:55 +0100

    add doCheck setting to coreutils-full.nix overlay and change override by overrideAttrs

--- configurations/modules/overlays/coreutils-full.nix
+++ configurations/modules/overlays/coreutils-full.nix
@@ -1,6 +1,16 @@
 self: super: {
-  coreutils-full = super.coreutils-full.override {
-    singleBinary = "no";
-  };
+ #coreutils-full = super.coreutils-full.override {
+ #  singleBinary = "no";
+ #};
+  
+  coreutils-full = super.coreutils-full.overrideAttrs (oldAttrs: {
+    configureFlags = [
+      "--with-packager=https://nixos.org"
+      "--disable-single-binary"
+      "--with-openssl"
+      "gl_cv_have_proc_uptime=yes"
+    ];
+    doCheck = false;
+  });
 }
 
