a88bf7ae34d97e62c05a6c15d0904870c907f298
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-12-13 15:43:38 +0100

    flake.nix: add lanzaboote for secureboot support

--- flake.nix
+++ flake.nix
@@ -9,13 +9,16 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }: 
+  outputs = inputs@{ self, nixpkgs, home-manager, lanzaboote, ... }: 
   let
     lib = nixpkgs.lib;
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
   in {
     nixosConfigurations = {
       hp-probook = lib.nixosSystem {
@@ -23,6 +26,20 @@
         modules = [
           ./configurations/configuration.nix
           ./profiles/hp-probook-profile.nix
+          lanzaboote.nixosModules.lanzaboote
+          ({ pkgs, lib, ... }: {
+            environment.systemPackages = with pkgs; [
+              sbctl
+            ];
+
+            boot = {
+              loader.systemd-boot.enable = lib.mkForce false;
+              lanzaboote = {
+                enable = true;
+                pkiBundle = "/etc/secureboot";
+              };
+            };
+          })
           home-manager.nixosModules.home-manager {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
