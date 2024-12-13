0062e04689e7edfe5ac36af9ba11243de9a7c89f
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2024-12-13 03:40:53 +0100

    many change for the bootloader module

--- profiles/vm-desktop-profile.nix
+++ profiles/vm-desktop-profile.nix
@@ -4,6 +4,7 @@
  ### Import nix expression for vm-desktop
  imports = 
    [ ../configurations/hardware-configuration/vm-desktop.nix        ### Hardware configuration file (Include the results of the hardware scan.)
+     ../configurations/modules/bootloader/systemd-boot.nix          ### Systemd-boot Bootloader
      ../configurations/modules/app-opts/vm-desktop.nix              ### Programs with options
      ../configurations/modules/networking/vm-desktop.nix            ### Related to network
      ../configurations/modules/system/tmp-on-disk.nix		    ### Use /tmp on disk
--- profiles/vm-no-gui-profile.nix
+++ profiles/vm-no-gui-profile.nix
@@ -4,6 +4,7 @@
  ### Import nix expression for vm-no-gui
  imports = 
    [ ../configurations/hardware-configuration/vm-no-gui.nix         ### Hardware configuration file (Include the results of the hardware scan.)
+     ../configurations/modules/bootloader/systemd-boot.nix          ### Systemd-boot Bootloader
      ../configurations/modules/app-opts/vm-no-gui.nix               ### Programs with options
      ../configurations/modules/networking/vm-no-gui.nix             ### Related to network
      ../configurations/modules/system/tmp-on-disk.nix		    ### Use /tmp on disk
