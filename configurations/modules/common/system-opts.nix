2334f2c9a6d3d50fc24cd4b768ad8e7920d1ad5e
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-01-08 14:08:54 +0100

    [nixpkgs/cbd8ec4d] modules/.../system-opts.nix: remove bash from runtime PATH for activationScript option (already use by activation script)

--- configurations/modules/common/system-opts.nix
+++ configurations/modules/common/system-opts.nix
@@ -43,11 +43,10 @@
  
  ### Nvd diff hook
  system.activationScripts.report-changes = ''
-   PATH="${pkgs.bash}/bin:${pkgs.coreutils}/bin:${pkgs.nix}/bin:${pkgs.nvd}/bin"
+   PATH="${pkgs.coreutils}/bin:${pkgs.nix}/bin:${pkgs.nvd}/bin"
    echo -e "\n===================================="
    echo      "| Running nvd diff to show changes |"
    echo -e   "====================================\n"
-   #nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link|tail -2)
    nvd diff /run/current-system $systemConfig
    echo ""
  '';
