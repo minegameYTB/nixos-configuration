0232ebd093801871b5557c520092b05107f68dbb
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-01-16 19:37:02 +0100

    [nixpkgs/635e887b] modules/.../gnome.nix: improve 'rice' preconfiguration and add adw-gtk3 theme to default theme for old legacy apps

--- configurations/modules/common/system-pkgs.nix
+++ configurations/modules/common/system-pkgs.nix
@@ -15,6 +15,7 @@
  # $ nix search wget
  environment.systemPackages = with pkgs; [
   #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   adw-gtk3 
    wget2
    nix-search-cli
    efibootmgr
