433c45bb71d66e05a14664dc65b58eb1941c47fe
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-01-22 17:29:51 +0100

    [nixpkgs/107d5ef0] flake.nix: add stylix remote flake

---
+++ home-manager/modules/default.nix
@@ -0,0 +1,7 @@
+{ ... }:
+
+{
+ imports =
+   [ ./stylix/stylix.nix
+   ];
+}
---
+++ home-manager/modules/stylix/stylix.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+
+{
+ stylix = {
+   enable = true;
+   image = ./theme/wallpaper/20240828_115530.jpg;
+  #polarity = "dark";
+ };
+}
