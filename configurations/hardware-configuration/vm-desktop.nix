f5ecde167116c65d693cfa7088d9a64c05c09787
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-01-24 00:42:03 +0100

    Update vm-desktop.nix

--- configurations/hardware-configuration/vm-desktop.nix
+++ configurations/hardware-configuration/vm-desktop.nix
@@ -18,7 +18,7 @@
       fsType = "ext4";
     };
 
-  fileSystems."/boot/efi" =
+  fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/009A-A77C";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
