6ac1faf6faeb84b4ea9a984aab9ebef8ea9a8a99
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-01-24 02:49:03 +0100

    update vm parts for this flake

--- profiles/vm-no-gui-profile.nix
+++ profiles/vm-no-gui-profile.nix
@@ -3,7 +3,7 @@
 {
  ### Import nix expression for vm-no-gui
  imports = 
-   [ ../configurations/hardware-configuration/vm-no-gui.nix         ### Hardware configuration file (Include the results of the hardware scan.)
+   [ ../configurations/hardware-configuration/vm.nix                ### Hardware configuration file (Include the results of the hardware scan.)
      ../configurations/modules/bootloader/systemd-boot.nix          ### Systemd-boot Bootloader
      ../configurations/modules/app-opts/vm-no-gui.nix               ### Programs with options
      ../configurations/modules/networking/vm-no-gui.nix             ### Related to network
