23badc52b7180a39f10cd9662c75f1e673bfed9a
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-01-30 16:21:43 +0100

    [nixos-24.11 - nixpkgs/2b4230bf] system/services: add isolate package for rm

---
+++ pkgs/rm-only/default.nix
@@ -0,0 +1,30 @@
+{ stdenvNoCC, coreutils }:
+
+let
+  ### Assigniate variable to rmBin
+  rmBin = "${coreutils}/bin/rm";
+in
+
+stdenvNoCC.mkDerivation {
+  pname = "rm-only";
+  version = coreutils.version;
+  
+  dontUnpack = true;
+  dontBuild = true;
+  dontConfigure = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${rmBin} $out/bin/rm
+  '';
+
+  ### Use only coreutils from local nix-store (defined on the header)
+  nativeBuildInputs = [ coreutils ];
+
+  ### Disable metadata
+ #meta = {
+ #  description = "Standalone 'rm' binary extracted from coreutils";
+ #  license = pkgs.lib.licenses.gpl3Plus;
+ #  platforms = pkgs.lib.platforms.all;
+ #};
+}
