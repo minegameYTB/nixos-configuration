e6de19084cc24a95a394728fb76bbd99be83fad3
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-02-09 02:23:39 +0100

    [nixpkgs/030ba197] security.nix: add gnupg option

--- configurations/modules/common/security.nix
+++ configurations/modules/common/security.nix
@@ -4,10 +4,11 @@
    # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-gnome3;
+   #enableSSHSupport = true;
+  };
   
  ### Apparmor
 #security.apparmor.enable = true;
