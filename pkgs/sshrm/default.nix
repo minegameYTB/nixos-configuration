1d9967fb03b5c941f951fff1508562cfe55ab916
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-02-22 04:49:27 +0100

    Update default.nix

--- pkgs/sshrm/default.nix
+++ pkgs/sshrm/default.nix
@@ -35,6 +35,6 @@ in
   postFixup = ''
     ### Add runtime path to sshrm tool
     wrapProgram $out/bin/${pname} \
-      --prefix PATH ${lib.makeBinPath [ sshUtilsOnly ]}
+      --set PATH ${lib.makeBinPath [ sshUtilsOnly ]}
   '';
  }
