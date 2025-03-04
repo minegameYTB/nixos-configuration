5c3a869b6df7b0da8ff1eac08a2a4e97061e96d0
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-03-05 00:38:25 +0100

    [nixpkgs/1546c45c] hardware-configuration/hp-probook.nix: disable swap

--- configurations/hardware-configuration/hp-probook.nix
+++ configurations/hardware-configuration/hp-probook.nix
@@ -36,9 +36,9 @@
     };
 
 
-  swapDevices =
-    [ { device = "/dev/disk/by-label/nixos-swap"; }
-    ];
+ #swapDevices =
+ #  [ { device = "/dev/disk/by-label/nixos-swap"; }
+ #  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
