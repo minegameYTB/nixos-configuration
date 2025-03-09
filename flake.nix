b619032c57f730f7113c04a6c8b75206c1801766
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-03-09 12:47:49 +0100

    [nixpkgs/20755fa0] flake.nix: move zen-browser and nur under home-manager source

--- flake.nix
+++ flake.nix
@@ -8,16 +8,16 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     
     ### Other repos
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
     nur = {
       url = "github:nix-community/nur";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+ 
     ### Other nixpkgs repos
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-23-11.url = "github:NixOS/nixpkgs/nixos-23.11";
