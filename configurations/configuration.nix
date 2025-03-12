127ab729347d15aa083ad2144b84ad1b3c69945f
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-03-12 09:36:33 +0100

    remove nfs mount

--- configurations/configuration.nix
+++ configurations/configuration.nix
@@ -11,9 +11,6 @@
      ./configs/common/system-pkgs.nix    ### System packages
      ./configs/common/timezone.nix       ### Timezone
      ./configs/common/security.nix       ### Security
-     
-     ### Add nfs mount
-     ./hardware-configuration/common/nfs-mount.nix
    ];
 
 ###----------------------------------------------------------------
