ca7d8947b994626df2528abe7976ee5037bd7479
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-04-17 05:37:50 +0200

    Update stylix.nix

--- configurations/configs/stylix/stylix.nix
+++ configurations/configs/stylix/stylix.nix
@@ -5,7 +5,10 @@
  stylix = {
    enable = true;
    ### Accept fetchurl derivation (see stylix doc)
-   image = ./wallpaper/firewatch.jpg;
+   image = pkgs.fetchurl {
+     url = "https://raw.githubusercontent.com/ChapST1/gruvbox-wallpapers-web/master/wallpapers/pixelart/9.png";
+     sha256 = "sha256-lfZ2g+DzBmQj8yHGmW1laDnhTEQtwvL75Dv9LLHgEQM=";
+   };
    polarity = "dark";
    cursor = {
      package = pkgs.catppuccin-cursors.mochaDark;
