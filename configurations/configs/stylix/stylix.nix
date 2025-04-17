d20cbaf26019951f09a6d218c13407936f29a41b
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-04-17 04:31:19 +0200

    made changes

--- configurations/configs/stylix/stylix.nix
+++ configurations/configs/stylix/stylix.nix
@@ -5,7 +5,7 @@
  stylix = {
    enable = true;
    ### Accept fetchurl derivation (see stylix doc)
-   image = ./wallpaper/Cherish.png;
+   image = ./wallpaper/firewatch.jpg;
    polarity = "dark";
    cursor = {
      package = pkgs.catppuccin-cursors.mochaDark;
