0a5f8c48be1836fd851e6963d36c76d76ecd5c7b
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-04-17 03:31:01 +0200

    disable plymoth (conflic with stylix)

--- configurations/configs/stylix/stylix.nix
+++ configurations/configs/stylix/stylix.nix
@@ -6,5 +6,10 @@
    enable = true;
    ### Accept fetchurl derivation (see stylix doc)
    image = ./wallpaper/Cherish.png;
+   polarity = "dark";
+   cursor = {
+     package = pkgs.catppuccin-cursors.mochaDark;
+     name = "catppuccin-mocha-dark-cursors";
+   };
  };
 }
