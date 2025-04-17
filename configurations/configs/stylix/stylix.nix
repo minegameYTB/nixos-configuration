bebe8f805c2ccf56f6d92c6f8bf42193933fc998
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-04-17 14:10:03 +0200

    add wallpaper from https://github.com/zhichaoh/catppuccin-wallpapers

--- configurations/configs/stylix/stylix.nix
+++ configurations/configs/stylix/stylix.nix
@@ -5,11 +5,13 @@
  stylix = {
    enable = true;
    ### Accept fetchurl derivation (see stylix doc)
-   image = pkgs.fetchurl {
-     url = "https://raw.githubusercontent.com/ChapST1/gruvbox-wallpapers-web/master/wallpapers/pixelart/9.png";
-     sha256 = "sha256-lfZ2g+DzBmQj8yHGmW1laDnhTEQtwvL75Dv9LLHgEQM=";
-   };
+   image = ./wallpaper/Cherish.png;
+  #image = pkgs.fetchurl {
+  #  url = "";
+  #  sha256 = "";
+  #};
    polarity = "dark";
+  #base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
    cursor = {
      package = pkgs.catppuccin-cursors.mochaDark;
      name = "catppuccin-mocha-dark-cursors";
