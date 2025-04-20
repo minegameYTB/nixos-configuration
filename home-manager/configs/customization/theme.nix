d9633b468619d093b73ef36407433df5cbc8bf39
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-04-20 20:25:56 +0200

    home-manager: add dotfiles from the another repos

--- home-manager/configs/customization/theme.nix
+++ home-manager/configs/customization/theme.nix
@@ -12,8 +12,11 @@
    ### Theme
    adw-gtk3
    papirus-icon-theme
+   
    ### This theme provide adwaita-dark theme (old theme)
    ayu-theme-gtk
+   
+   ### From my nurpkgs repo
    nur.repos.minegameYTB.theme.marble-shell-filled
  ];
 
