81ed8dbe43718d724de34a49257e904a360bbfec
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-04-23 20:54:42 +0200

    fonts.nix: init fonts.nix (and remove these option from app-opts folder)

--- configurations/configs/app-opts/hp-probook.nix
+++ configurations/configs/app-opts/hp-probook.nix
@@ -25,12 +25,5 @@
      set linenumbers 
    '';
  };
-
- ### NerdFonts
- fonts.packages = with pkgs; [
-   (nerdfonts.override { fonts = [ "UbuntuMono" ]; })
- ];
- ### Replace actual syntax on 25.05 by:
- #fonts.packages = with pkgs; [ nerd-fonts.ubuntu-mono ];
-
+ 
 }
