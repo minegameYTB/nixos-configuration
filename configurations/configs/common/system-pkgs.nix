f4f00df089af597494f3b21815b4f236647eec92
minegameYTB <53137994+minegameYTB@users.noreply.github.com>
2025-04-28 16:24:10 +0200

    made changes

--- configurations/configs/common/system-pkgs.nix
+++ configurations/configs/common/system-pkgs.nix
@@ -11,9 +11,6 @@
   #'';
  in
 {
- # Allow unfree packages
- nixpkgs.config.allowUnfree = true;
-
  nixpkgs.overlays = [
   #(import ../overlays/coreutils-full.nix) ### Overlays for coreutils-full (just compile tools for users and not runtime deps for software)
   #(import ../overlays/appimage-run.nix) ### Overlays to add some tools on appimage-run rootfs
