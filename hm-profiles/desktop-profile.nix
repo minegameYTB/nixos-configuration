c0707173bb43eb35ab484cb696949df9dabc50c3
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-04-28 05:18:44 +0200

    home-manager: reformat import style in desktop/server profiles

--- hm-profiles/desktop-profile.nix
+++ hm-profiles/desktop-profile.nix
@@ -6,11 +6,11 @@
  home.homeDirectory = "/home/${username}";
 
  ### Import nix expression for desktop
- imports = 
-   [ ../home-manager/home.nix                                   ### Common configuration
-     ../home-manager/configs/customization/theme.nix            ### Related to theme configuration
-     ../home-manager/configs/customization/apps.nix             ### Related to app config
-     ../home-manager/configs/customization/cli-app.nix          ### Related to cli software configuration
-     ../home-manager/configs/desktop/gui-packages.nix           ### Related to GUI packages (need to use with a DE)
-   ];
+ imports = [ 
+   ../home-manager/home.nix                                   ### Common configuration
+   ../home-manager/configs/customization/theme.nix            ### Related to theme configuration
+   ../home-manager/configs/customization/apps.nix             ### Related to app config
+   ../home-manager/configs/customization/cli-app.nix          ### Related to cli software configuration
+   ../home-manager/configs/desktop/gui-packages.nix           ### Related to GUI packages (need to use with a DE)
+ ];
 }
