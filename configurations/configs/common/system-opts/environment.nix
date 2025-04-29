140ce9d89782ce6382df2d7b59fd0b76a635b1e2
minegameYTB <53137994+minegameYTB@users.noreply.github.com>
2025-04-29 18:01:15 +0200

    system-opts: split system-opts.nix to system-opts folder

---
+++ configurations/configs/common/system-opts/environment.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+ ### Common env variable
+ environment.variables = {
+   EDITOR = "nvim";
+ };
+
+ ### Shell environment
+ environment.shellAliases = {
+   ls = "lsd";
+   cat = "bat";
+   df = "df -x tmpfs";
+   w-df = "watch df -hx tmpfs";
+   "cat.ori" = "/run/booted-system/sw/bin/cat";
+   "ls.ori" = "/run/booted-system/sw/bin/ls";
+   which = "/run/current-system/sw/bin/which";
+   ff = "fastfetch";
+   nix = "nix -v --refresh";
+   gc = "git commit";
+   gadd = "git add";
+   gpush = "git push";
+   gpull = "git pull";
+
+   ### Use xterm-256color on runtime command
+   ssh = "TERM=xterm-256color ssh";
+
+   ### This alias is just inspired from macOS "open" command
+   open = "xdg-open";
+ };
+}
---
+++ configurations/configs/common/system-opts/nix-settings.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+ ### Nix Settings
+ nix = {
+   ### Directory relative to channel are removed with the service "nix-channel-rm-dirs.service"
+   channel.enable = false;
+  #registry.nix-custom-repo.to = {
+  #  owner = "minegameYTB";
+  #  repo = "nix-custom-repo";
+  #  type = "github";
+  #};
+   settings = {
+     warn-dirty = false;
+     auto-optimise-store = true;
+     experimental-features = [ "nix-command" "flakes" ];
+     max-jobs = 2;
+   };
+   gc = {
+     automatic = true;
+     dates = "weekly";
+     options = "--delete-older-than 7d";
+   };
+   optimise = {
+     automatic = true;
+     dates = [ "weekly" ];
+   };
+ };
+
+ ### Disable some nixos other command
+ system.tools = {
+   nixos-option.enable = false;
+   nixos-build-vms.enable = false;
+   nixos-install.enable = false;
+ };
+}
