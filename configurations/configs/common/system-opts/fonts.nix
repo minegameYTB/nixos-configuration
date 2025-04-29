5ec4df07f5f2bba14bb69af90f43c81c54afba08
minegameYTB <53137994+minegameYTB@users.noreply.github.com>
2025-04-29 18:09:22 +0200

    system-opts: move system-opts.nix to default.nix and import all expression of this folder

---
+++ configurations/configs/common/system-opts/fonts.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+
+{
+ ### NerdFonts
+ fonts.packages = with pkgs; [
+   (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+ ];
+ 
+ ### Replace actual syntax on 25.05 by:
+ #fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
+}
