9d45807cbd65d8d66923b3b3473a2a6402b49f41
minegameYTB <53137994+minegameYTB@users.noreply.github.com>
2025-04-30 23:38:15 +0200

    home-manager: remove stylix conf

--- home-manager/configs/customization/apps.nix
+++ home-manager/configs/customization/apps.nix
@@ -7,13 +7,4 @@
    "ghostty/config".source = "${inputs.dotfiles-minegameYTB}/configs/ghostty/config";
  };
  
- ### stylix
- stylix = {
-   enable = true;
-   image = "${inputs.catppuccin-wallpapers}/landscapes/tropic_island_morning.jpg";
-   targets = {
-     tmux.enable = false;
-   };
- };
- 
 }
