4d102cec4b2151f6c2cc479d10ad7b9f98b55403
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-05-06 04:12:01 +0200

    home-manager: remove bash and zsh configuration

--- home-manager/configs/common/config.nix
+++ home-manager/configs/common/config.nix
@@ -31,19 +31,6 @@
    };
  };
 
- ### Aliases
- home.shellAliases = {
-   ff = "fastfetch";
-   nix = "nix -v --refresh";
-   gc = "git commit";
-   gadd = "git add";
-   gpush = "git push";
-   gpull = "git pull";
-
-   ### This alias is just inspired from macOS "open" command
-   open = "xdg-open";
- };
-
  ### GH
  programs.gh = {
    enable = true;
@@ -108,52 +95,52 @@
  };
 
  ### bash
- programs.bash = {
-   enable = true;
-   initExtra = ''
-     # Provide a nice prompt if the terminal supports it.
-     if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
-       PROMPT_COLOR="1;31m"
-       ((UID)) && PROMPT_COLOR="1;32m"
-       if [ -n "$INSIDE_EMACS" ]; then
-         # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
-         PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
-       else
-         PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
-       fi
-       if test "$TERM" = "xterm"; then
-         PS1="\[\033]2;\h:\u:\w\007\]$PS1"
-       fi
-     fi
-   '';
- };
+ #programs.bash = {
+ #  enable = true;
+ #  initExtra = ''
+ #    # Provide a nice prompt if the terminal supports it.
+ #    if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
+ #      PROMPT_COLOR="1;31m"
+ #      ((UID)) && PROMPT_COLOR="1;32m"
+ #      if [ -n "$INSIDE_EMACS" ]; then
+ #        # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+ #        PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+ #      else
+ #        PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+ #      fi
+ #      if test "$TERM" = "xterm"; then
+ #        PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+ #      fi
+ #    fi
+ #  '';
+ #};
 
  ### zsh
- programs.zsh = {
-   enable = true;
-   syntaxHighlighting = {
-     enable = true;
-     highlighters = [
-       "main"
-     ];
-     styles = {
-       "comment" = "fg=red,bold";
-       "unknown-token" = "fg=red";
-     };
-   };
-   autosuggestion = {
-     enable = true;
-     strategy = [ "match_prev_cmd" ];
-   };
-   oh-my-zsh = {
-     enable = true;
-     theme = "agnoster";
-   };
+ #programs.zsh = {
+ #  enable = true;
+ #  syntaxHighlighting = {
+ #    enable = true;
+ #    highlighters = [
+ #      "main"
+ #    ];
+ #    styles = {
+ #      "comment" = "fg=red,bold";
+ #      "unknown-token" = "fg=red";
+ #    };
+ #  };
+ #  autosuggestion = {
+ #    enable = true;
+ #    strategy = [ "match_prev_cmd" ];
+ #  };
+ #  oh-my-zsh = {
+ #    enable = true;
+ #    theme = "agnoster";
+ #  };
    ### Replace initExtra by InitContent (after upgrade to home-manager/nixos-25.05)
-   initExtra = ''
-     export NIXPKGS_COMMIT=$(curl -s https://raw.githubusercontent.com/minegameYTB/nixos-configuration/flake/flake.lock | jq -r '.nodes."nixpkgs".locked.rev' | cut -c1-8)
-   '';
- };
+ #  initExtra = ''
+ #    export NIXPKGS_COMMIT=$(curl -s https://raw.githubusercontent.com/minegameYTB/nixos-configuration/flake/flake.lock | jq -r '.nodes."nixpkgs".locked.rev' | cut -c1-8)
+ #  '';
+ #};
 
  ### Fonts
  home.packages = with pkgs; [
