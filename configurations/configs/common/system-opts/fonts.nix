6a773216068378ab9d0a9cfa60d5256c0befb886
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-05-07 21:55:34 +0200

    fonts.nix: made changes

--- configurations/configs/common/system-opts/fonts.nix
+++ configurations/configs/common/system-opts/fonts.nix
@@ -2,9 +2,9 @@
 
 {
  ### NerdFonts
- fonts.packages = with pkgs; [
-   (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
- ];
+ #fonts.packages = with pkgs; [
+ #  (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+ #];
  
  ### Replace actual syntax on 25.05 by:
  #fonts.packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
