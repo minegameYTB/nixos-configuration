ef73fd8687bd4b97bb8175e387e6180195ad89a2
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-05-07 02:36:02 +0200

    hardware-configuration: use label instead of device option (same result)

--- configurations/hardware-configuration/hp-240/hardware-configuration.nix
+++ configurations/hardware-configuration/hp-240/hardware-configuration.nix
@@ -4,43 +4,44 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+ imports = [ 
+   (modulesPath + "/installer/scan/not-detected.nix")
+ ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ ];
+ boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
+ boot.initrd.kernelModules = [ ];
+ boot.kernelModules = [ "kvm-intel" ];
+ boot.extraModulePackages = with config.boot.kernelPackages; [ ];
 
-  fileSystems."/" = { 
-    device = "/dev/disk/by-label/nixos-root";
-    fsType = "btrfs";
-    options = [ "subvol=@" "compress=zstd:10" "noatime" ];
-  };
+ fileSystems."/" = { 
+   #device = "/dev/disk/by-label/nixos-root";
+   label = "nixos-root";
+   fsType = "btrfs";
+   options = [ "subvol=@" "compress=zstd:10" "noatime" ];
+ };
 
-  fileSystems."/home" = { 
-    device = "/dev/disk/by-label/nixos-root";
-    fsType = "btrfs";
-    options = [ "subvol=@home" "compress=zstd:10" "noatime" ];
-  };
+ fileSystems."/home" = { 
+   label = "nixos-root";
+   fsType = "btrfs";
+   options = [ "subvol=@home" "compress=zstd:10" "noatime" ];
+ };
 
-  fileSystems."/nix" = { 
-    device = "/dev/disk/by-label/nixos-root";
-    fsType = "btrfs";
-    options = [ "subvol=@nix" "compress=zstd:10" "noatime" ];
-  };
+ fileSystems."/nix" = { 
+   label = "nixos-root";
+   fsType = "btrfs";
+   options = [ "subvol=@nix" "compress=zstd:10" "noatime" ];
+ };
 
-  swapDevices = [ ];
+ swapDevices = [ ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+ # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+ # (the default) this is the recommended approach. When using systemd-networkd it's
+ # still possible to use this option, but it's recommended to use it in conjunction
+ # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+ networking.useDHCP = lib.mkDefault true;
+ # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
+ # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+ nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+ hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
