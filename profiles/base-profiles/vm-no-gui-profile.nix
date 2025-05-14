04b9eff8a02531c6ffab6339a23c538af0737e00
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-05-14 05:54:02 +0200

    profiles:change path of hardware-configuration for vm profile

--- profiles/base-profiles/vm-no-gui-profile.nix
+++ profiles/base-profiles/vm-no-gui-profile.nix
@@ -3,11 +3,11 @@
 {
  ### Import nix expression for vm-no-gui
  imports = [ 
-   ../../configurations/hardware-configuration/vm.nix                ### Hardware configuration file (Include the results of the hardware scan.)
-   ../../configurations/configs/app-opts/vm-no-gui.nix               ### Programs with options
-   ../../configurations/configs/networking/server-config.nix         ### Related to network
-   ../../configurations/configs/system/tmp-on-disk.nix               ### Use /tmp on disk
-   ../../configurations/configs/specific/vm/guest/qemu-kvm-guest.nix ### To use optimisation of qemu/kvm
-   ../../configurations/configs/specific/vm/guest/openssh.nix        ### Enable openssh service
+   ../../configurations/hardware-configuration/vm/hardware-configuration.nix ### Hardware configuration file (Include the results of the hardware scan.)
+   ../../configurations/configs/app-opts/vm-no-gui.nix                       ### Programs with options
+   ../../configurations/configs/networking/server-config.nix                 ### Related to network
+   ../../configurations/configs/system/tmp-on-disk.nix                       ### Use /tmp on disk
+   ../../configurations/configs/specific/vm/guest/qemu-kvm-guest.nix         ### To use optimisation of qemu/kvm
+   ../../configurations/configs/specific/vm/guest/openssh.nix                ### Enable openssh service
  ];
 }
