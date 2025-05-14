344fd665c9a5dbdbe8d8606bfbcb0241740c2017
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-05-14 22:40:17 +0200

    profile/base-profiles: made same changes like hp-probook and hp-240

--- profiles/base-profiles/vm-desktop-profile.nix
+++ profiles/base-profiles/vm-desktop-profile.nix
@@ -2,17 +2,30 @@
 
 {
  ### Import nix expression for vm-desktop
- imports = [ 
-   ../../configurations/hardware-configuration/vm/hardware-configuration.nix                    ### Hardware configuration file (Include the results of the hardware scan.)
-   ../../configurations/configs/app-opts/vm-desktop.nix                  ### Programs with options
-   ../../configurations/configs/networking/desktop-config.nix             ### Related to network
-   ../../configurations/configs/system/tmp-on-disk.nix                   ### Use /tmp on disk
-   ../../configurations/configs/specific/desktop/gnome.nix               ### Related to GNOME DE
-   ../../configurations/configs/specific/desktop/x11.nix                 ### Related to x11 Server (GUI server)
-   ../../configurations/configs/specific/vm/guest/qemu-kvm-guest.nix     ### To use optimisation of qemu/kvm
-   ../../configurations/configs/specific/desktop/sound.nix               ### Sound server
-   #../../configurations/configs/specific/desktop/autologin.nix          ### Permit autologin
-   ../../configurations/configs/system/services/nix-channel-rm-dirs.nix  ### Related to remove nix-channel folder (unused on my case)
-   ../../configurations/configs/stylix/stylix.nix                        ### Stylix conf
+ imports = [
+   ### Core
+   ../../configurations/hardware-configuration/vm/hardware-configuration.nix
+   ../../configurations/configs/app-opts/vm-desktop.nix
+   ../../configurations/configs/networking/desktop-config.nix
+   ../../configurations/configs/system/tmp-on-disk.nix
+   
+   ### X11 and desktop environment
+   ../../configurations/configs/specific/desktop/gnome.nix
+   ../../configurations/configs/specific/desktop/sound.nix
+   #../../configurations/configs/specific/desktop/autologin.nix
+   #../../configurations/configs/specific/desktop/printer.nix
+   
+   ### Services
+   ../../configurations/configs/system/services/nix-channel-rm-dirs.nix
+   
+   ### Vm specific option
+   ../../configurations/configs/specific/vm/guest/qemu-kvm-guest.nix
+   
+   ### Other
+   ../../configurations/configs/app-opts/vm-desktop.nix
+   
+   ### NixOS configuration module (distant flake)
+   ### import default.nix from this directory ↓
+   ../../configurations/config-modules
  ];
 }
--- profiles/base-profiles/vm-no-gui-profile.nix
+++ profiles/base-profiles/vm-no-gui-profile.nix
@@ -2,12 +2,19 @@
 
 {
  ### Import nix expression for vm-no-gui
- imports = [ 
-   ../../configurations/hardware-configuration/vm/hardware-configuration.nix ### Hardware configuration file (Include the results of the hardware scan.)
-   ../../configurations/configs/app-opts/vm-no-gui.nix                       ### Programs with options
-   ../../configurations/configs/networking/server-config.nix                 ### Related to network
-   ../../configurations/configs/system/tmp-on-disk.nix                       ### Use /tmp on disk
-   ../../configurations/configs/specific/vm/guest/qemu-kvm-guest.nix         ### To use optimisation of qemu/kvm
-   ../../configurations/configs/specific/vm/guest/openssh.nix                ### Enable openssh service
+ imports = [
+   ### Core
+   ../../configurations/hardware-configuration/vm/hardware-configuration.nix
+   ../../configurations/configs/networking/server-config.nix
+   ../../configurations/configs/system/tmp-on-disk.nix
+   
+   ### Vm specific option
+   ../../configurations/configs/specific/vm/guest/qemu-kvm-guest.nix
+   
+   ### Server specific
+   ../../configurations/configs/specific/vm/guest/openssh.nix
+   
+   ### Other
+   ../../configurations/configs/app-opts/vm-no-gui.nix
  ];
 }
