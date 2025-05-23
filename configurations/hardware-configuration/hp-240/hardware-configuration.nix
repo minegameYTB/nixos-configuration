b59a01b05c804de4de8c37ae5b9cab6456917c7e
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-05-23 18:23:36 +0200

    change hardware configuration logic for real pc (not vm for now)

--- configurations/hardware-configuration/hp-240/hardware-configuration.nix
+++ configurations/hardware-configuration/hp-240/hardware-configuration.nix
@@ -13,35 +13,5 @@
  boot.kernelModules = [ "kvm-intel" ];
  boot.extraModulePackages = with config.boot.kernelPackages; [ ];
 
- fileSystems."/" = { 
-   #device = "/dev/disk/by-label/nixos-root";
-   label = "nixos-root";
-   fsType = "btrfs";
-   options = [ "subvol=@" "compress=zstd:5" "noatime" ];
- };
-
- fileSystems."/home" = { 
-   label = "nixos-root";
-   fsType = "btrfs";
-   options = [ "subvol=@home" "compress=zstd:5" "noatime" ];
- };
-
- fileSystems."/nix" = { 
-   label = "nixos-root";
-   fsType = "btrfs";
-   options = [ "subvol=@nix" "compress=zstd:5" "noatime" ];
- };
-
  swapDevices = [ ];
-
- # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
- # (the default) this is the recommended approach. When using systemd-networkd it's
- # still possible to use this option, but it's recommended to use it in conjunction
- # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
- networking.useDHCP = lib.mkDefault true;
- # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
- # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
-
- nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
- hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
