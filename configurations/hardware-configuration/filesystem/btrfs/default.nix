2c1199ecf4f203bfaf7cfb4fa13e655e7419d518
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-05-29 22:04:28 +0200

    hardware-configuration: add ext4 tools

--- configurations/hardware-configuration/filesystem/btrfs/default.nix
+++ configurations/hardware-configuration/filesystem/btrfs/default.nix
@@ -25,6 +25,11 @@
    options = [ "subvol=@nix" "compress=zstd:5" "noatime" ];
  };
 
+ boot.supportedFilesystems = {
+   btrfs = true;
+   ext4 = true; 
+ };
+
  ### Network stack
  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
  # (the default) this is the recommended approach. When using systemd-networkd it's
