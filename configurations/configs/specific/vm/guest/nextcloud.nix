ec468183078d01df5163bdebcc278da5f74f048c
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-01 17:00:37 +0200

    Update nextcloud.nix

--- configurations/configs/specific/vm/guest/nextcloud.nix
+++ configurations/configs/specific/vm/guest/nextcloud.nix
@@ -6,7 +6,7 @@
  services.nextcloud = {
    enable = true;
    package = pkgs.nextcloud31;
-   hostName = "192.168.1.127";
+   hostName = "localhost";
    config.adminpassFile = "/etc/nextcloud-admin-pass";
    config.dbtype = "sqlite";
  };
