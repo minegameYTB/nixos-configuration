9db3584e871fe9433245775f4e20375be54974ae
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-01 16:22:28 +0200

    Update nextcloud.nix

--- configurations/configs/specific/vm/guest/nextcloud.nix
+++ configurations/configs/specific/vm/guest/nextcloud.nix
@@ -7,9 +7,31 @@
    enable = true;
    hostName = "nixos-nextcloud";
    package = pkgs.nextcloud31;
+   configureRedis = true;
    config = {
      adminpassFile = "/etc/nextcloud-admin-pass";
-     dbtype = "sqlite";
+     dbtype = "pgsql";
+     dbuser = "nextcloud";
+     dbhost = "/run/postgresql";
+     dbname = "nextcloud";
    };
+ };
+ 
+ ### Setup postgresql (for nextcloud)
+ services.postgresql = {
+   enable = true;
+   ensureDatabases = [ "nextcloud" ];
+   ensureUsers = [
+     {
+       name = "nextcloud";
+       ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
+     }
+   ];
+ };
+ 
+ # ensure that postgres is running *before* running the setup
+ systemd.services."nextcloud-setup" = {
+   requires = ["postgresql.service"];
+   after = ["postgresql.service"];
  };
 }
