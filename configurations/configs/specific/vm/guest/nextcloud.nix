f3963024d0d3e0fafa3e0026d85821141c1d7e0f
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-01 16:43:06 +0200

    Update nextcloud.nix

--- configurations/configs/specific/vm/guest/nextcloud.nix
+++ configurations/configs/specific/vm/guest/nextcloud.nix
@@ -2,42 +2,12 @@
 
 {
  ### Nextcloud
- environment.etc."nextcloud-admin-pass".text = "nixos";
+ environment.etc."nextcloud-admin-pass".text = "PWD";
  services.nextcloud = {
    enable = true;
-   hostName = "nixos-nextcloud";
    package = pkgs.nextcloud31;
-   configureRedis = true;
-   config = {
-     adminpassFile = "/etc/nextcloud-admin-pass";
-     dbtype = "pgsql";
-     dbuser = "nextcloud";
-     dbhost = "/run/postgresql";
-     dbname = "nextcloud";
-   };
- };
- 
- ### Setup postgresql (for nextcloud)
- services.postgresql = {
-   enable = true;
-   ensureDatabases = [ "nextcloud" ];
-   ### Temporary passwd (sops-nix needed later)
-   initialScript = pkgs.writeText "nextcloud-init.sql" ''
-     CREATE ROLE nextcloud WITH LOGIN PASSWORD 'motdepasse';
-     CREATE DATABASE nextcloud WITH OWNER nextcloud;
-     GRANT ALL PRIVILEGES ON DATABASE nextcloud TO nextcloud;
-   '';
-   ensureUsers = [
-     {
-       name = "nextcloud";
-       #ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
-     }
-   ];
- };
- 
- # ensure that postgres is running *before* running the setup
- systemd.services."nextcloud-setup" = {
-   requires = ["postgresql.service"];
-   after = ["postgresql.service"];
+   hostName = "localhost";
+   config.adminpassFile = "/etc/nextcloud-admin-pass";
+   config.dbtype = "sqlite";
  };
 }
