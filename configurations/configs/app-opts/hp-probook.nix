d5abc02a5539e99af288d1ff1d5e13c8f2f14199
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-04 22:15:59 +0200

    profiles: remove Steam config from hp-probook

--- configurations/configs/app-opts/hp-probook.nix
+++ configurations/configs/app-opts/hp-probook.nix
@@ -4,14 +4,6 @@
  ### Programs with options
  ### Firefox
  programs.firefox.enable = true;
-  
- ### Steam
- programs.steam = {
-   enable = true;
-   extraCompatPackages = [
-     pkgs.proton-ge-bin
-   ];
- };
 
  ### Localsend
  programs.localsend.enable = true;
