7144b947b0fd244ace9a41b3778be2d7e0142a08
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-04 21:43:21 +0200

    move gnome. and plasma.nix to environment

--- profiles/hp-240-profile.nix
+++ profiles/hp-240-profile.nix
@@ -15,7 +15,7 @@
    #../configurations/configs/specific/laptop/power-mgmt.nix
    
    ### X11 and desktop environment
-   ../configurations/configs/specific/desktop/gnome.nix
+   ../configurations/configs/specific/desktop/environment/gnome.nix
    ../configurations/configs/specific/desktop/sound.nix
    #../configurations/configs/specific/desktop/printer.nix
    #../configurations/configs/specific/desktop/autologin.nix
