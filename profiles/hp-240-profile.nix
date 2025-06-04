51eb67f45b62b1dfd93e55e326b391253840a9d6
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-05 01:50:31 +0200

    made changes

--- profiles/hp-240-profile.nix
+++ profiles/hp-240-profile.nix
@@ -25,7 +25,6 @@
 
    ### Vm specific option
    #../configurations/configs/specific/vm/host/qemu-kvm-host.nix
-    ../configurations/configs/specific/vm/guest/openssh.nix
 
    ### Games specific
    ../configurations/configs/specific/desktop/games/steam/steam-run-free.nix
