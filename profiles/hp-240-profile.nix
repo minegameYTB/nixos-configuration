b0ee33ae6d0cc2f654722a6ed5d523d0222cd852
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-05 01:52:12 +0200

    profiles: enable autologin for hp-240

--- profiles/hp-240-profile.nix
+++ profiles/hp-240-profile.nix
@@ -18,7 +18,7 @@
    ../configurations/configs/specific/desktop/environment/gnome.nix
    ../configurations/configs/specific/desktop/sound.nix
    #../configurations/configs/specific/desktop/printer.nix
-   #../configurations/configs/specific/desktop/autologin.nix
+   ../configurations/configs/specific/desktop/autologin.nix
    
    ### Services
    ../configurations/configs/system/services/nix-channel-rm-dirs.nix
