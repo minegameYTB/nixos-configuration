df8f44a9591e9e6d3894b99c7b80cd50d9822479
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-05 02:10:40 +0200

    profile/vm-desktop-profile.nix: reintroduced autologin module config

--- profiles/base-profiles/vm-desktop-profile.nix
+++ profiles/base-profiles/vm-desktop-profile.nix
@@ -13,7 +13,7 @@
    ### X11 and desktop environment
    ../../configurations/configs/specific/desktop/environment/gnome.nix
    ../../configurations/configs/specific/desktop/sound.nix
-   #../../configurations/configs/specific/desktop/autologin.nix
+   ../../configurations/configs/specific/desktop/autologin.nix
    #../../configurations/configs/specific/desktop/printer.nix
    
    ### Services
