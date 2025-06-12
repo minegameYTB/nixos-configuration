b5eb189b3fb5d148ca647f2c76e301a8d6813e07
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-12 16:42:42 +0200

    build.sh: change nixpkgs hash (nixpkgs-25.05)

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash --pure
 #! nix-shell -p gnumake nix nixfmt-rfc-style statix deadnix findutils unzip git jq cacert
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/1807c2b91223227ad5599d7067a61665c52d1295.tar.gz
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/88331c17ba434359491e8d5889cce872464052c2.tar.gz
 
 ### A wrapper to run make, without need to install make, nix will add make to the runtime PATH
 make $1
