96235f8c13b87af43c7ea547491922f4442624e4
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-17 04:32:07 +0200

    Update hp-probook-profile.nix

--- profiles/hp-probook-profile.nix
+++ profiles/hp-probook-profile.nix
@@ -16,7 +16,7 @@
  
    ### X11 and desktop environment
    ../configurations/configs/specific/desktop/environment/gnome.nix
-   #../configurations/configs/specific/desktop/plasma.nix
+   #../configurations/configs/specific/desktop/environment/plasma.nix
    ../configurations/configs/specific/desktop/sound.nix
    #../configurations/configs/specific/desktop/printer.nix
 
