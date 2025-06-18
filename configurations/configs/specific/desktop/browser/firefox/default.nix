ed22f055c554ecaf56363021ac05cb80ebb7e183
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-18 03:31:48 +0200

    init hardening for browsers

---
+++ configurations/configs/specific/desktop/browser/firefox/default.nix
@@ -0,0 +1,27 @@
+{ lib, config, pkgs, ... }:
+
+{
+ ### Firefox
+ programs.firefox = {
+   enable = true;
+   wrapperConfig.pipewireSupport = true;
+   languagePacks = [
+     "fr"
+     "en-US"
+   ];
+   preferences = {
+     "intl.accept_languages" = "fr-fr,en-us,en";
+     "intl.locale.requested" = "fr,en-US";
+   };
+ };
+
+ ### Firejail configuration for firefox (enabled on ../default.nix)
+ programs.firejail.wrappedBinaries = {
+   firefox = {
+     ### Refer binary with lib.getExe (see https://nixos.org/manual/nixpkgs/stable/#function-library-lib.meta.getExe-prime)
+     executable = "${lib.getExe pkgs.firefox}";
+     #profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
+     extraArgs = [ "--disable-mnt" "--private-tmp" ];
+   };
+ };
+}
