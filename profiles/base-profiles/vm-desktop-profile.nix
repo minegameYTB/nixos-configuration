eff2409004bea4b909dc2d05a5fbe3b738973231
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-18 21:13:24 +0200

    profile: fixing sddm autologin priority error (need testing)

--- profiles/base-profiles/vm-desktop-profile.nix
+++ profiles/base-profiles/vm-desktop-profile.nix
@@ -11,6 +11,7 @@
    
    ### X11 and desktop environment
    ../../configurations/configs/specific/desktop/environment/gnome.nix
+   #../../configurations/configs/specific/desktop/environment/plasma.nix
    ../../configurations/configs/specific/desktop/sound.nix
    ../../configurations/configs/specific/desktop/autologin.nix
    #../../configurations/configs/specific/desktop/printer.nix
