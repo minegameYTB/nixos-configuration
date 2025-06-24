3e61a595e9f37aabc2daa20021659a7c1b65d320
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-24 02:48:59 +0200

    home-manager: fix whitespace/indentation in custom-pkgs.nix

--- home-manager/configs/common/custom-pkgs.nix
+++ home-manager/configs/common/custom-pkgs.nix
@@ -8,14 +8,14 @@
 {
  home.packages = 
    (with nurpkgs-repo-minegameYTB.legacyPackages.${pkgs.stdenvNoCC.hostPlatform.system}; [
-    ### Custom packages
-    ### Add custom-pkgs from my repo (nurpkgs-repo)
-    sshrm
-    editor.msedit-rs
+     ### Custom packages
+     ### Add custom-pkgs from my repo (nurpkgs-repo)
+     sshrm
+     editor.msedit-rs
    ])
    ++
    (with pkgs; [
      ### With nur namespace
      #nur.repos.minegameYTB.sshrm
-]);
+ ]);
 }
