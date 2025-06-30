44c7f643bc5843982cadf587e84054d0893a9607
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-06-30 05:00:30 +0200

    Update custom-pkgs.nix

--- home-manager/configs/common/custom-pkgs.nix
+++ home-manager/configs/common/custom-pkgs.nix
@@ -9,14 +9,14 @@
  home.packages = 
    (with pkgs; [
      ### With nur namespace (nixpkgs stable)
-     #nur.repos.minegameYTB.sshrm
+     nur.repos.minegameYTB.sshrm
      #nur.repos.minegameYTB.editor.msedit-rs
  ])
  ++
    (with nurpkgs-repo-minegameYTB.legacyPackages.${pkgs.stdenvNoCC.hostPlatform.system}; [
      ### Custom packages
      ### Add custom-pkgs from my repo (nurpkgs-repo) (nixpkgs unstable)
-     sshrm
+     #sshrm
      editor.msedit-rs
  ]);
 }
