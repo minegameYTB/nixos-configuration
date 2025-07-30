88743e79fd652183c7b2ef8ad5e9e20ec1cb93c2
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-07-30 21:02:47 +0200

    stylix: change wallpaper

--- configurations/config-modules/stylix/default.nix
+++ configurations/config-modules/stylix/default.nix
@@ -7,7 +7,7 @@
  ### Stylix config
  stylix = {
    enable = true;
-   image = "${inputs.catppuccin-wallpapers}/misc/lonely-fish.png";
+   image = "${inputs.dotfiles-minegameYTB}/wallpapers/Custom/Matt-manual-upscale-1920x1080.png";
    
    ### Catppucin wallpaper
    #"${inputs.catppuccin-wallpapers}/landscapes/Cloudsnight.jpg";
