af16755c863ce759c84aebf67ebf327875de3bfd
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-08-08 03:52:26 +0200

    home-manager: add GLFfetch-glfos package and remove pkgsExtra for adwaita font

--- home-manager/configs/common/custom-pkgs.nix
+++ home-manager/configs/common/custom-pkgs.nix
@@ -11,6 +11,7 @@
      ### With nur namespace (nixpkgs stable)
      nur.repos.minegameYTB.sshrm
      nur.repos.minegameYTB.editor.msedit
+     nur.repos.minegameYTB.GLFfetch-glfos
  ]);
  #++
  #  (with nurpkgs-repo-minegameYTB.legacyPackages.${pkgs.stdenvNoCC.hostPlatform.system}; [
