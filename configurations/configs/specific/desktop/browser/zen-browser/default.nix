759de532642c837de5c24a33c6974e6644d41973
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-08-13 04:18:19 +0200

    modules: temporairy disable custom and add zen-browser to home-manager env (and global env)

--- configurations/configs/specific/desktop/browser/zen-browser/default.nix
+++ configurations/configs/specific/desktop/browser/zen-browser/default.nix
@@ -8,8 +8,11 @@
 
 {
  ### Install zen-browser from custom modules (see /configurations/modules/programs/zen-browser-modules.nix for all options)
- programs.zen-browser = {
-   enable = true;
-   enableHardening = true;
- };
+ #programs.zen-browser = {
+ #  enable = true;
+ #  enableHardening = true;
+ #};
+
+ ### Install zen-browser from the flake directly
+ environment.systemPackages = with pkgs; [ zen-browser.packages."${pkgs.stdenvNoCC.hostPlatform.system}".default ];
 }
