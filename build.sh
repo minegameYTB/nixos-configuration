abbcfdbfe27d486d4e8a072060e313d25dae6a65
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-08-18 20:24:05 +0200

    build.sh: change some dependancy

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash --pure
-#! nix-shell -p gnumake nix nixfmt-rfc-style statix deadnix findutils unzip git jq cacert
+#! nix-shell -p gnumake nix.out nixfmt-rfc-style statix deadnix findutils unzip gitMinimal jq cacert
 #! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/88331c17ba434359491e8d5889cce872464052c2.tar.gz
 
 ### A wrapper to run make, without need to install make, nix will add make to the runtime PATH
