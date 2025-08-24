e5060ec2d734bcfe4e0d2d59b4d87e56e9167533
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-08-24 13:47:29 +0200

    made changes

--- configurations/configs/bootloader/systemd-boot.nix
+++ configurations/configs/bootloader/systemd-boot.nix
@@ -6,19 +6,6 @@
    systemd-boot = {
      enable = true;
      configurationLimit = 10;
-     
-     ### Enable memtest86+
-     extraFiles = {
-       "efi/memtest86/memtest.efi" = "${pkgs.memtest86plus}/memtest.efi";
-     };
-     extraEntries = {
-       "memtest86.conf" = ''
-         title Memtest86+
-         efi /efi/memtest86/memtest.efi
-         sort-key z_memtest
-       '';
-     };
-     memtest86.enable = true;
    };
    efi.canTouchEfiVariables = true;
  };  
