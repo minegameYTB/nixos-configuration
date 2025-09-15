a2edd3ef2f92fc70c49a063bbf4286858d4acdc6
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-09-15 23:31:12 +0200

    made changes

--- configurations/configs/common/system-pkgs.nix
+++ configurations/configs/common/system-pkgs.nix
@@ -17,43 +17,36 @@ in
    #(import ../overlays/package-name.nix)
  ];
 
- environment.systemPackages = 
+ environment.systemPackages = (with pkgs; [
+   ### CLI
+   wget
+   nix-search-cli
+   efibootmgr
+   ntfs3g
+   nvd
+
+   ### Pass $TERM env variable to ssh via a shell wrapper (and export openssh path as a global path for ssh wrapper)
+   (pkgs.writeShellScriptBin "ssh" ''
+     export PATH='${lib.getBin pkgs.openssh}'
+     export TERM='xterm-256color'
+     exec -a "$0" ${config.programs.ssh.package}/bin/ssh "$@"
+   '')
+   #nixos-rebuild
+ ]) ++ (with pkgsExtra.pkgs-unstable; [
+   ### Extra packages always installed (from pkgsExtra)
+   #ventoy
+ ]) ++ (lib.optionals config.services.xserver.enable (
    (with pkgs; [
-     ### CLI
-     wget
-     nix-search-cli
-     efibootmgr
-     ntfs3g
-     nvd
+     ### GUI Packages (only if X11 is enabled)
+     gparted
+     gearlever
+     onlyoffice-desktopeditors
 
-     ### Pass $TERM env variable to ssh via a shell wrapper (and export openssh path as a global path for ssh wrapper)
-     (pkgs.writeShellScriptBin "ssh" ''
-       export PATH='${lib.getBin pkgs.openssh}'
-       export TERM='xterm-256color'
-       exec -a "$0" ${config.programs.ssh.package}/bin/ssh "$@"
-     '')
-     #nixos-rebuild
+     ### Ghostty
+     ghostty.packages.${pkgs.stdenvNoCC.hostPlatform.system}.default
+   ]) ++ (with pkgsExtra.pkgs-unstable; [
+     ### Extra GUI packages from pkgsExtra (only if X11 is enabled)
+     #bottles
    ])
-   ++
-   (with pkgsExtra.pkgs-unstable; [
-     ### Extra packages always installed (from pkgsExtra)
-     #ventoy
-   ])
-   ++
-   (lib.optionals config.services.xserver.enable (
-     (with pkgs; [
-       ### GUI Packages (only if X11 is enabled)
-       gparted
-       gearlever
-       onlyoffice-desktopeditors
-
-       ### Ghostty
-       ghostty.packages.${pkgs.stdenvNoCC.hostPlatform.system}.default
-     ])
-     ++
-     (with pkgsExtra.pkgs-unstable; [
-       ### Extra GUI packages from pkgsExtra (only if X11 is enabled)
-       #bottles
-     ])
  ));
 }
