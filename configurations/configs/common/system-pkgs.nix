6be8b32fc733d023a16ce4951df74e48c4212996
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-09-18 01:06:35 +0200

    Revert "system-pkgs.nix: move github-desktop, spotify and deezer-enhanced package to globale conf (don't work on non NixOS system (replace it with flatpak ver on home-manager standalone))"
    This reverts commit 8cfac92075ae04b3e823c2e2619dac4b389a4d03.

--- configurations/configs/common/system-pkgs.nix
+++ configurations/configs/common/system-pkgs.nix
@@ -34,14 +34,11 @@ in
      gparted
      gearlever
      onlyoffice-desktopeditors
-     github-desktop
-     spotify
 
      ### Ghostty
      ghostty.packages.${pkgs.stdenvNoCC.hostPlatform.system}.default
    ]) ++ (with pkgsExtra.pkgs-unstable; [
      ### Extra GUI packages from pkgsExtra (only if X11 is enabled)
-     deezer-enhanced
      #bottles
    ])
  ));
