96dab828de4e0f430ab61d682c29fbea76213767
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-09-17 23:09:46 +0200

    home-manager standalone: made change

--- home-manager/configs/specific/standalone/config-modules/declarative-flatpak/default.nix
+++ home-manager/configs/specific/standalone/config-modules/declarative-flatpak/default.nix
@@ -20,7 +20,7 @@
      ### Search package with this command (for all used info)
    # {remote}:{type}/{ref}/[{arch}]/{branch}[:{commit}]
 
-     "flathub:app/io.github.shiftey.Desktop//stable"
+     #"flathub:app/io.github.shiftey.Desktop//stable"
      "flathub:app/io.mrarm.mcpelauncher//stable"
      #"flathub:app/com.spotify.Client//stable"
      #"com.usebottles.bottles"
