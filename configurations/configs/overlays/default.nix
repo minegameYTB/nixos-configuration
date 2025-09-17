bfee09d8e132be02014db9dcae967eb6be129536
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-09-17 18:43:29 +0200

    configuration.nix: add overlays expression for global software

---
+++ configurations/configs/overlays/default.nix
@@ -0,0 +1,12 @@
+{ lib, pkgs, ... }:
+
+{
+ ### Import package overlays (and patch) on global software
+ nixpkgs.overlays = [
+   #(import ../overlays/coreutils-full.nix)
+   #(import ../overlays/appimage-run.nix)
+   #(import ../overlays/gnome-control-center.nix)
+   (import ../overlays/gnome-mutter.nix)
+   #(import ../overlays/package-name.nix)
+ ];
+}
