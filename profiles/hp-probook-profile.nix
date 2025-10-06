57d2bb74f17d8380e38a29eeb9f5f54128e5fc87
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-10-07 01:07:44 +0200

    secure-boot: disable secure boot bootloader for hp-probook

--- profiles/hp-probook-profile.nix
+++ profiles/hp-probook-profile.nix
@@ -37,6 +37,6 @@
    ../configurations/config-modules
 
    ### Add lanzaboote (separate module)
-   ../configurations/config-modules/lanzaboote
+   #../configurations/config-modules/lanzaboote
  ];
 }
