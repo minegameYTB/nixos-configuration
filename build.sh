43915103a7b070021b47d3e1be64cdea9dad5481
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-11-30 12:28:34 +0100

    build.sh: change commit by nixos-25.11-beta

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash --pure
 #! nix-shell -p gnumake nix.out unzip gitMinimal jq cacert
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/50a96edd8d0db6cc8db57dab6bb6d6ee1f3dc49a.tar.gz
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/1dcdcf9efc6aed8bf28347c0bfa583ba511954ae.tar.gz
 
 ### A wrapper to run make, without need to install make, nix will add make to the runtime PATH
 make $1
