9593bd604b5eafee0ec67695554ed603668a78e4
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-12-01 15:30:31 +0100

    qemu-host.nix: disable chmod qemu-libvirt service

--- configurations/configs/specific/vm/host/qemu-kvm-host.nix
+++ configurations/configs/specific/vm/host/qemu-kvm-host.nix
@@ -1,79 +1,84 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 
 {
- ### Virt-manager
- programs.virt-manager.enable = true;
+  ### Virt-manager
+  programs.virt-manager.enable = true;
 
- ### Vm specific package
- environment.systemPackages = with pkgs; [ libguestfs ];
+  ### Vm specific package
+  environment.systemPackages = with pkgs; [ libguestfs ];
 
- ### KSM ram optimisation
- hardware.ksm.enable = true;
+  ### KSM ram optimisation
+  hardware.ksm.enable = true;
 
- ### Virtualisation settings
- virtualisation = {
-   kvmgt.enable = true;
-   spiceUSBRedirection.enable = true;
-   libvirtd = {
-     enable = true;
-     
-     shutdownTimeout = 90;
-     onShutdown = "shutdown";
-     onBoot = "ignore";
-     qemu = {
-       ### Define package for vm
-       package = pkgs.qemu_kvm;
+  ### Virtualisation settings
+  virtualisation = {
+    kvmgt.enable = true;
+    spiceUSBRedirection.enable = true;
+    libvirtd = {
+      enable = true;
 
-       ### Tpm support in qemu
-       swtpm.enable = true;
-       
-       ### Run qemu vm in qemu-libvirtd user instead of root
-       runAsRoot = false;
-     };
-   };
- };
+      shutdownTimeout = 90;
+      onShutdown = "shutdown";
+      onBoot = "ignore";
+      qemu = {
+        ### Define package for vm
+        package = pkgs.qemu_kvm;
 
- ### Temporary systemd service (delete later)
- systemd.services."qemu-libvirtd-chmod" = {
-   enable = config.virtualisation.libvirtd.qemu.runAsRoot != true;
-   description = "Define 'qemu-libvirtd' user as a new owner of '/var/lib/libvirt/qemu/*' directory";
-   wantedBy = [ "multi-user.target" ];
-   
-   ### Strictly use coreutils package for this service (+ all hardening used) (differant of systemd.services.<name>.path, this option add to path instead of overwrite the path)
-   environment.PATH = lib.mkForce "${pkgs.coreutils}/bin";
-   serviceConfig = {
-     Type = "oneshot";
-     
-     ### Run service as root (for chmod)
-     User = "root";
-     
-     ### Hardening service (to only touch /var/lib/libvirt/qemu/* dir)
-     ProtectSystem = "strict";
-     ProtectHome = "read-only";
-     PrivateTmp = "true";
-     NoNewPrivileges = "yes";
-   };
-   script = ''
-     ### see description of "virtualisation.libvirtd.qemu.runAsRoot" for more info
-     targetDir="/var/lib/libvirt/qemu"
-     newUser="qemu-libvirtd"
-     currentUser=$(stat -c "%U" "$targetDir")
-     
-     ### test if root is the owner of targetDir
-     if [ "$currentOwner" = "$newUser" ]; then
-       exit 0
-     fi
-     
-     ### apply new owner if it's root (folder)
-     chown -R $newUser:$newUser $targetDir
+        ### Tpm support in qemu
+        swtpm.enable = true;
 
-     ### apply new owner on all directory of this directory
-     chown -R $newUser $targetDir/*
-   '';
- };
+        ### Run qemu vm in qemu-libvirtd user instead of root
+        runAsRoot = false;
+      };
+    };
+  };
 
- ### Nix specific
- virtualisation.vmVariant = {
+  ### Temporary systemd service (delete later)
+  #systemd.services."qemu-libvirtd-chmod" = {
+  #  enable = config.virtualisation.libvirtd.qemu.runAsRoot != true;
+  # description = "Define 'qemu-libvirtd' user as a new owner of '/var/lib/libvirt/qemu/*' directory";
+  # wantedBy = [ "multi-user.target" ];
+
+  ### Strictly use coreutils package for this service (+ all hardening used) (differant of systemd.services.<name>.path, this option add to path instead of overwrite the path)
+  # environment.PATH = lib.mkForce "${pkgs.coreutils}/bin";
+  # serviceConfig = {
+  #   Type = "oneshot";
+
+  ### Run service as root (for chmod)
+  #   User = "root";
+
+  ### Hardening service (to only touch /var/lib/libvirt/qemu/* dir)
+  #   ProtectSystem = "strict";
+  #   ProtectHome = "read-only";
+  #   PrivateTmp = "true";
+  #  NoNewPrivileges = "yes";
+  # };
+  #  script = ''
+  ### see description of "virtualisation.libvirtd.qemu.runAsRoot" for more info
+  #   targetDir="/var/lib/libvirt/qemu"
+  #   newUser="qemu-libvirtd"
+  #   currentUser=$(stat -c "%U" "$targetDir")
+
+  ### test if root is the owner of targetDir
+  #   if [ "$currentOwner" = "$newUser" ]; then
+  #     exit 0
+  #   fi
+
+  ### apply new owner if it's root (folder)
+  #   chown -R $newUser:$newUser $targetDir
+
+  ### apply new owner on all directory of this directory
+  #   chown -R $newUser $targetDir/*
+  # '';
+  #};
+
+  ### Nix specific
+  virtualisation.vmVariant = {
     # following configuration is added only when building VM with build-vm
     virtualisation = {
       memorySize = 1024; # Use 1024MiB memory.
