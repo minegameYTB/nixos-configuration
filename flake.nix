07e18cd27f95a50cdc32863fc23b19a68cb75d64
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-12-03 15:51:23 +0100

    Revert "flake.nix: add comment for stylix disabling"
    This reverts commit 2741e7d17a964e9f979762feefc1b799d935308d.

--- flake.nix
+++ flake.nix
@@ -196,7 +196,6 @@
               username:
               import ./hm-profiles/desktop-profile-wrapped.nix {
                 inherit username;
-                ### Stylix is referenced in ./home-manager/configs/specific/nixos/default.nix
                 extraModules = [ ./home-manager/configs/specific/nixos ];
               }
             );
