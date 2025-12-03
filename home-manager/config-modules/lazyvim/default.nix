bee822f11af61b18eb2bd1c0a4a638a24f6d1507
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-12-03 19:08:09 +0100

    lazyvim: use git package from programs.git.package option

--- home-manager/config-modules/lazyvim/default.nix
+++ home-manager/config-modules/lazyvim/default.nix
@@ -21,12 +21,17 @@
     extras = {
       lang.nix.enable = true;
     };
-    extraPackages = with pkgs; [
-      nixd # Nix LSP
-      nixfmt # Nix formatter
-      statix # For suggestion on nix files
-      gcc # Provides the C Compiler
-      tree-sitter # Provides the tree-sitter CLI
-    ];
+    extraPackages =
+      (with pkgs; [
+        nixd # Nix LSP
+        nixfmt # Nix formatter
+        statix # For suggestion on nix files
+        gcc # Provides the C Compiler
+        tree-sitter # Provides the tree-sitter CLI
+        feh # show jpg image in terminal
+      ])
+      ++
+      ### Use git package from programs.git.package
+      [ config.programs.git.package ];
   };
 }
