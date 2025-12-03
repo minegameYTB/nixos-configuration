2c23f8c876460effce7b83c573387b8156ffe035
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2025-12-03 03:23:57 +0100

    flake: add ccache

--- configurations/config-modules/stylix/default.nix
+++ configurations/config-modules/stylix/default.nix
@@ -1,51 +1,66 @@
-{ config, pkgs, inputs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
- ### Import stylix modules like a expression
- imports = [ inputs.stylix.nixosModules.stylix ];
+  ### Import stylix modules like a expression
+  imports = [ inputs.stylix.nixosModules.stylix ];
 
- ### Stylix config
- stylix = {
-   enable = true;
-   image = "${inputs.dotfiles-minegameYTB}/wallpapers/Custom/Matt-manual-upscale-1920x1080.png";
-   
-   ### Catppucin wallpaper
-   #"${inputs.catppuccin-wallpapers}/landscapes/Cloudsnight.jpg";
-  
-   ### Accept fetchurl derivation (see stylix doc)
-   #image = pkgs.fetchurl {
-   #  url = "";
-   #  sha256 = "";
-   #};
-   polarity = "dark";
-   fonts = {
-     sansSerif = {
-       ### Use pkgsExtra.pkgs-unstable to get adwaita-fonts (even if i use nixpkgs stable by default)
-       package = pkgs.adwaita-fonts;
-       name = "Adwaita Sans";
-     };
-     serif = {
-       package = pkgs.adwaita-fonts;
-       name = "Adwaita Sans";
-     };
-     monospace = {
-       package = pkgs.adwaita-fonts;
-       name = "Adwaita Mono";
-     };
-     sizes.applications = 11;
-   };
-  #base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-   cursor = {
-     ### Use nixpkgs stable for this package
-     package = pkgs.catppuccin-cursors.mochaDark;
-     name = "catppuccin-mocha-dark-cursors";
-     size = 24;
-   };
-   targets = {
-     plymouth = {
-       enable = true;
-       logoAnimated = false;
-     };
-   };
- };
+  ### Ccache related options
+  programs.ccache.packageNames = [
+    "gdm"
+    "gnome-shell"
+    "gnome-text-editor"
+    "gnome-calculator"
+    "gnome-session"
+    "gtksourceview5"
+  ];
+
+  ### Stylix config
+  stylix = {
+    enable = true;
+    image = "${inputs.dotfiles-minegameYTB}/wallpapers/Custom/Matt-manual-upscale-1920x1080.png";
+
+    ### Catppucin wallpaper
+    #"${inputs.catppuccin-wallpapers}/landscapes/Cloudsnight.jpg";
+
+    ### Accept fetchurl derivation (see stylix doc)
+    #image = pkgs.fetchurl {
+    #  url = "";
+    #  sha256 = "";
+    #};
+    polarity = "dark";
+    fonts = {
+      sansSerif = {
+        ### Use pkgsExtra.pkgs-unstable to get adwaita-fonts (even if i use nixpkgs stable by default)
+        package = pkgs.adwaita-fonts;
+        name = "Adwaita Sans";
+      };
+      serif = {
+        package = pkgs.adwaita-fonts;
+        name = "Adwaita Sans";
+      };
+      monospace = {
+        package = pkgs.adwaita-fonts;
+        name = "Adwaita Mono";
+      };
+      sizes.applications = 11;
+    };
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    cursor = {
+      ### Use nixpkgs stable for this package
+      package = pkgs.catppuccin-cursors.mochaDark;
+      name = "catppuccin-mocha-dark-cursors";
+      size = 24;
+    };
+    targets = {
+      plymouth = {
+        enable = true;
+        logoAnimated = false;
+      };
+    };
+  };
 }
--- configurations/configs/common/system-opts/nix-settings.nix
+++ configurations/configs/common/system-opts/nix-settings.nix
@@ -1,48 +1,67 @@
-{ config, pkgs, pkgsExtra, inputs, ... }:
+{
+  config,
+  pkgs,
+  pkgsExtra,
+  inputs,
+  ...
+}:
 
 {
- ### Nix Settings
- nix = {
-   ### Use nix from ctrl os
-   package = pkgsExtra.pkgs-lts.nix;
+  ### Ccache
+  programs.ccache = {
+    enable = true;
+    cacheDir = "/nix/var/cache/ccache";
 
-   ### Directory relative to channel are removed with the service "nix-channel-rm-dirs.service"
-   channel.enable = false;
-  #registry.nix-custom-repo.to = 
-  #  owner = "minegameYTB";
-  #  repo = "nix-custom-repo";
-  #  type = "github";
-  #};
-   settings = {
-     warn-dirty = false;
-     auto-optimise-store = true;
-     experimental-features = [ "nix-command" "flakes" "ca-derivations" ];
-     max-jobs = 2;
-     cores = 2;
-   };
-   gc = {
-     automatic = true;
-     dates = "weekly";
-     options = "--delete-older-than 7d";
-   };
-   optimise = {
-     automatic = true;
-     dates = [ "weekly" ];
-   };
- };
+    ### move packageNames option to some directory (stylix for exemple)
+  };
 
- ### Ctrl-os substitutes (custom option (defined in /configurations/modules/nix/ctrl-os-substitutes.nix))
- ctrl-os.substitutes.enable = true;
+  ### Nix Settings
+  nix = {
+    ### Use nix from ctrl os
+    package = pkgsExtra.pkgs-lts.nix;
 
- system = {
-   ### Enable nixos-rebuild-ng to replace nixos-rebuild legacy (take "config.system.tools.nixos-rebuild.enable" value to use value defind in this option as true)
-   #rebuild.enableNg = config.system.tools.nixos-rebuild.enable; ### Remove this line in 26.05 release
+    ### Directory relative to channel are removed with the service "nix-channel-rm-dirs.service"
+    channel.enable = false;
+    #registry.nix-custom-repo.to =
+    #  owner = "minegameYTB";
+    #  repo = "nix-custom-repo";
+    #  type = "github";
+    #};
+    settings = {
+      warn-dirty = false;
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "ca-derivations"
+      ];
+      extra-sandbox-paths = [ config.programs.ccache.cacheDir ];
+      max-jobs = 2;
+      cores = 2;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    optimise = {
+      automatic = true;
+      dates = [ "weekly" ];
+    };
+  };
 
-   ### Disable some nixos other command
-   tools = {
-     nixos-option.enable = false;
-     nixos-build-vms.enable = false;
-     nixos-install.enable = false;
-   };
- };
+  ### Ctrl-os substitutes (custom option (defined in /configurations/modules/nix/ctrl-os-substitutes.nix))
+  ctrl-os.substitutes.enable = true;
+
+  system = {
+    ### Enable nixos-rebuild-ng to replace nixos-rebuild legacy (take "config.system.tools.nixos-rebuild.enable" value to use value defind in this option as true)
+    #rebuild.enableNg = config.system.tools.nixos-rebuild.enable; ### Remove this line in 26.05 release
+
+    ### Disable some nixos other command
+    tools = {
+      nixos-option.enable = false;
+      nixos-build-vms.enable = false;
+      nixos-install.enable = false;
+    };
+  };
 }
