d0f256124561c07f09507415a40c69285ac219af
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-01-02 01:13:23 +0100

    systemd-boot.nix: add memtest86plus entry

--- configurations/configs/bootloader/systemd-boot.nix
+++ configurations/configs/bootloader/systemd-boot.nix
@@ -9,7 +9,21 @@
     systemd-boot = {
       enable = true;
       configurationLimit = 10;
+
+      ### Enable Memtest86+ entry for all systemd-boot based configuration
+      # Add entry
+      extraEntries."memtest86plus.conf" = ''
+        title Memtest86+
+        efi /efi/memtest86plus/memtest.efi
+        sort-key z_memtest
+      '';
+
+      # Export memtest86plus to $BOOT directory
+      extraFiles = {
+        "efi/memtest86plus/memtest.efi" = "${pkgs.memtest86plus}/memtest.efi";
+      };
     };
+    ### Enable EFI editable variable
     efi.canTouchEfiVariables = true;
   };
 }
