079537c9387f86dc40dd66bf2d7807b674d5700e
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-01-08 22:58:25 +0100

    steam: add sdl2 to the steam environment

--- configurations/configs/specific/desktop/games/steam/default.nix
+++ configurations/configs/specific/desktop/games/steam/default.nix
@@ -1,9 +1,21 @@
-{ config, pkgsExtra, ... }:
+{
+  config,
+  pkgs,
+  pkgsExtra,
+  ...
+}:
 
 {
   ### Steam (already provide steam-run (unfree))
   programs.steam = {
     enable = true;
+    package = pkgs.steam.override {
+      extraLibraries =
+        # variable can be nammed with other name
+        lib: with lib; [
+          SDL2
+        ];
+    };
     extraCompatPackages = [
       pkgsExtra.pkgs-unstable.proton-ge-bin
     ];
