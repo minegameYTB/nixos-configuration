210df2fd847b78b816503082e4a5300b8faa9916
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-01-18 03:28:20 +0100

    declarative-flatpak: remove TinyWiiBackupManager app

--- configurations/config-modules/declarative-flatpak/default.nix
+++ configurations/config-modules/declarative-flatpak/default.nix
@@ -22,7 +22,7 @@
 
       #"flathub:app/io.github.shiftey.Desktop//stable"
       "flathub:app/io.mrarm.mcpelauncher//stable"
-      "flathub:app/it.mq1.TinyWiiBackupManager//stable"
+      #"flathub:app/it.mq1.TinyWiiBackupManager//stable"
       #"com.usebottles.bottles"
     ];
   };
