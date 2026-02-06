a484ad70028c5dffc7ec651cc8a93de6625656ae
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-02-06 01:29:04 +0100

    fonts.nix: add fonts

--- configurations/configs/common/system-opts/fonts.nix
+++ configurations/configs/common/system-opts/fonts.nix
@@ -6,10 +6,15 @@
   #  (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   #];
 
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
 
-    ### Japanese font
-    noto-fonts-cjk-sans
-  ];
+      ### Japanese font
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-color-emoji
+    ];
+    fontDir.enable = true;
+  };
 }
