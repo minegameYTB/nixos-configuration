dfb3c512c21a071809b24105ec399a108c5c524a
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-02-08 01:33:15 +0100

    hardware-configuration: add btrfs scrub configuration

--- configurations/hardware-configuration/filesystem/btrfs/default.nix
+++ configurations/hardware-configuration/filesystem/btrfs/default.nix
@@ -38,6 +38,13 @@
     ];
   };
 
+  ### Btrfs scrub maintenance
+  services.btrfs.autoScrub = {
+    enable = true;
+    fileSystems = [ "/" ];
+    interval = "monthly";
+  };
+
   boot.supportedFilesystems = {
     btrfs = true;
     ext4 = true;
