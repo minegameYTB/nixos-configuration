739860442bc2b7308e44e3d735db727fac8a0761
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-02-10 00:15:41 +0100

    flake.nix: remove nixGL

--- overlay.nix
+++ overlay.nix
@@ -12,7 +12,6 @@ with inputs;
   nixpkgs.overlays = [
     ### Extend pkgs with nur namespace
     nur.overlays.default
-    nixgl.overlay
 
     ### Custom extend of pkgs or replacing pkgs by other
     (self: super: rec {
