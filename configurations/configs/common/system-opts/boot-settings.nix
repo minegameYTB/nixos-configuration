4902624083a32bae1c5c15734a1a44497b60fb27
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-02-10 16:34:42 +0100

    boot-settings.nix: add hardening settings

--- configurations/configs/common/system-opts/boot-settings.nix
+++ configurations/configs/common/system-opts/boot-settings.nix
@@ -6,7 +6,7 @@
 }:
 
 {
-  ### Boot config
+  ### Boot config (followed this guide for hardening: https://madaidans-insecurities.github.io/guides/linux-hardening.html)
   boot = {
     binfmt = {
       preferStaticEmulators = true;
@@ -18,18 +18,68 @@
       enable = true;
       emergencyAccess = "$y$j9T$CmuNpg/fSyEMO8pehMLwU.$Oe7w2sKzs6teBwP5rU.OOVeGyMAHKL8Pz3JunPlLOv/";
     };
+    consoleLogLevel = 0;
     kernelParams = [
       "quiet"
-      "boot.shell_on_fail"
+
+      ### Hardening
+      "slab_nomerge"
+      "init_on_alloc=1"
+      "init_on_free=1"
+      "page_alloc.shuffle=1"
+      "pti=on"
+      "vsyscall=none"
+      "debugfs=off"
+      "module.sig_enforce=1"
+      "lockdown=integrity"
     ];
     kernel.sysctl = {
+      # QoL settings
       "kernel.panic" = 10;
       "vm.swappiness" = 10;
-      "kernel.printk" = "3 3 3 3";
 
-      # Hardening
+      # Hardening (kernel)
+      "kernel.printk" = "3 3 3 3";
       "kernel.kptr_restrict" = 2;
       "kernel.kexec_load_disabled" = 1;
+      "kernel.perf_event_paranoid" = 3;
+      "kernel.dmesg_restrict" = 1;
+      "kernel.unprivileged_bpf_disabled" = 1;
+      "kernel.yama.ptrace_scope" = 1;
+
+      # Hardening (filesystem)
+      "fs.protected_symlinks" = 1;
+      "fs.protected_hardlinks" = 1;
+      "fs.protected_fifos" = 2;
+      "fs.protected_regular" = 2;
+
+      # Hardening (memory)
+      "vm.mmap_rnd_bits" = 32;
+      "vm.mmap_rnd_compat_bits" = 16;
+      "vm.unprivileged_userfaultfd" = 0;
+
+      # Hardening (network)
+      "net.core.bpf_jit_enable" = 0;
+      "net.ipv4.tcp_syncookies" = 1;
+      "net.ipv4.tcp_rfc1337" = 1;
+
+      "net.ipv4.conf.all.rp_filter" = 1;
+      "net.ipv4.conf.default.rp_filter" = 1;
+
+      "net.ipv4.conf.all.accept_redirects" = 0;
+      "net.ipv4.conf.default.accept_redirects" = 0;
+      "net.ipv4.conf.all.secure_redirects" = 0;
+      "net.ipv4.conf.default.secure_redirects" = 0;
+      "net.ipv6.conf.all.accept_redirects" = 0;
+      "net.ipv6.conf.default.accept_redirects" = 0;
+
+      "net.ipv4.conf.all.send_redirects" = 0;
+      "net.ipv4.conf.default.send_redirects" = 0;
+
+      "net.ipv4.conf.all.accept_source_route" = 0;
+      "net.ipv4.conf.default.accept_source_route" = 0;
+      "net.ipv6.conf.all.accept_source_route" = 0;
+      "net.ipv6.conf.default.accept_source_route" = 0;
     };
     kernelPackages = pkgs.linuxKernel.packages.linux_6_18;
   };
