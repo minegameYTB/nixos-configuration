22cb7aeea4f4176566ae3bc9e564be8215d84d1f
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-02-11 21:04:49 +0100

    system-pkgs.nix: add man-pages package

--- configurations/configs/common/system-pkgs.nix
+++ configurations/configs/common/system-pkgs.nix
@@ -10,42 +10,44 @@
 {
   environment.systemPackages =
     (with pkgs; [
-      ### CLI
+      # CLI
       wget
       efibootmgr
       ntfs3g
       nvd
 
-      ### Pass $TERM env variable to ssh via a shell wrapper (and export openssh path as a global path for ssh wrapper)
+      # Man pages
+      man-pages
+
+      # Pass $TERM env variable to ssh via a shell wrapper (and export openssh path as a global path for ssh wrapper)
       (pkgs.writeShellScriptBin "ssh" ''
         export PATH='${lib.getBin config.programs.ssh.package}'
         export TERM='xterm-256color'
         exec -a "$0" ${config.programs.ssh.package}/bin/ssh "$@"
       '')
-      #nixos-rebuild
     ])
     ++ (with pkgs.pkgsUnstable; [
-      ### Extra packages always installed (from pkgs.pkgsUnstable)
+      # Extra packages always installed (from pkgs.pkgsUnstable)
       #ventoy
     ])
     ++ (
       lib.optionals config.services.xserver.enable (
         with pkgs;
         [
-          ### GUI Packages (only if X11 is enabled)
+          # GUI Packages (only if X11 is enabled)
           gparted
           gearlever
           onlyoffice-desktopeditors
           github-desktop
           rpi-imager
 
-          ### Ghostty
+          # Ghostty
           #ghostty.packages.${pkgs.stdenvNoCC.hostPlatform.system}.default
           ghostty
         ]
       )
       ++ (with pkgs.pkgsUnstable; [
-        ### Extra GUI packages from pkgs.pkgsUnstable (only if X11 is enabled)
+        # Extra GUI packages from pkgs.pkgsUnstable (only if X11 is enabled)
         #bottles
       ])
     );
