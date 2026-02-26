188c20a6aa03e9b437073fea1ba874c469a7ef97
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-02-26 23:59:52 +0100

    lanzaboote: change service

--- configurations/config-modules/lanzaboote/default.nix
+++ configurations/config-modules/lanzaboote/default.nix
@@ -30,48 +30,36 @@
   };
 
   ### Clean .auth files created in /
-  systemd.services.cleanup-lanzaboote-auth = {
+  systemd.services."cleanup-lanzaboote-auth" = {
     description = "Clean unused new .auth files in /";
     environment = lib.mkForce {
-      PATH = "${pkgs.efivar.bin}/bin:${pkgs.hexdump}/bin:${pkgs.coreutils.out}/bin:${pkgs.gawk.out}/bin";
+      PATH = "${pkgs.efivar.bin}/bin:${pkgs.coreutils.out}/bin:${pkgs.gawk.out}/bin";
     };
     wantedBy = [ "multi-user.target" ];
     after = [ "efivarfs.mount" ];
     unitConfig.ConditionPathExistsGlob = "/{PK,KEK,db}.auth";
     serviceConfig = {
-      Type = "oneshot";
+      User = "root";
+      #Type = "oneshot";
 
       ### Hardening service
-      ProtectSystem = "strict";
+      ProtectSystem = "full";
       PrivateTmp = "true";
+      PrivateDevices = "true";
+      ProtectKernelLogs = "true";
+      ProtectKernelModules = "yes";
+      ProtectHostname = "true";
+      ProtectHome = "true";
+      ProtectProc = "invisible";
+      PrivateUsers = "true";
+      PrivateNetwork = "true";
+      PrivateMounts = "true";
       NoNewPrivileges = "yes";
-      ReadWritePaths = [
-        "/PK.auth"
-        "/KEK.auth"
-        "/db.auth"
-      ];
     };
     script = ''
-      SB_FILE=$(ls /sys/firmware/efi/efivars/SecureBoot-* 2>/dev/null | head -n1)
-      SM_FILE=$(ls /sys/firmware/efi/efivars/SetupMode-* 2>/dev/null | head -n1)
-
-      if [ -z "$SB_FILE" ] || [ -z "$SM_FILE" ]; then
-        echo "Unable to find SetupMode, Skipping"
-        exit 0
-      fi
-
-      secureBoot=$(hexdump -v -e '1/1 "%d"' "$SB_FILE" | tail -c 1)
-      setupMode=$(hexdump -v -e '1/1 "%d"' "$SM_FILE" | tail -c 1)
-
-      echo "SecureBoot=$secureBoot SetupMode=$setupMode"
-
-      if [ "$secureBoot" = "1" ] && [ "$setupMode" = "0" ]; then
-        echo "Provisioning complete. Cleaning up .auth files..."
-        rm -f /PK.auth /KEK.auth /db.auth
-        echo "Cleanup done."
-      else
-        echo "Provisioning not complete. Skipping cleanup"
-      fi
+      echo "Cleaning up .auth files..."
+      rm -f /PK.auth /KEK.auth /db.auth
+      echo "Cleanup done."
     '';
   };
 }
