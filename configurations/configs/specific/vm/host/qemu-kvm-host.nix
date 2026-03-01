f41c464843003499518f92df37c8db5a28feafd9
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-03-01 20:43:45 +0100

    change module blacklist and remove unused settings and modules

--- configurations/configs/specific/vm/host/qemu-kvm-host.nix
+++ configurations/configs/specific/vm/host/qemu-kvm-host.nix
@@ -15,13 +15,6 @@
   ### KSM ram optimisation
   hardware.ksm.enable = true;
 
-  ### Specific kernel configuration
-  boot.extraModprobeConfig = ''
-    options kvm_intel nested=1
-    options kvm_intel emulate_invalid_guest_state=0
-    options kvm ignore_msrs=1 report_ignored_msrs=0
-  '';
-
   ### Virtualisation settings
   virtualisation = {
     kvmgt.enable = true;
