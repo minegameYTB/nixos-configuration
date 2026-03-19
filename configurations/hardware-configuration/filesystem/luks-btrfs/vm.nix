b20db8f09e995ea07dcd0f74e410bad5fa89a2aa
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-03-19 16:11:15 +0100

    luks-btrfs: fix path for vm specific expression

--- configurations/hardware-configuration/filesystem/luks-btrfs/vm.nix
+++ configurations/hardware-configuration/filesystem/luks-btrfs/vm.nix
@@ -1,10 +1,15 @@
-{ config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 
 {
   ### Include original luks partitionning and override keyFile option
   imports = [ ./default.nix ];
 
   boot.initrd.luks.devices."luks-encrypted" = {
-    keyFile = "/dev/disk/by-id/mmc-APPSD_0x00000354-part1";
+    keyFile = "/dev/vdb1";
   };
 }
