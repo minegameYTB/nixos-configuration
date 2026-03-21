882aa0e64860933751fbe753b0c375577555b5f7
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-03-21 20:05:02 +0100

    qemu-kvm-host.nix: disable some option to libvirt package

--- configurations/configs/specific/vm/host/qemu-kvm-host.nix
+++ configurations/configs/specific/vm/host/qemu-kvm-host.nix
@@ -21,6 +21,10 @@
     spiceUSBRedirection.enable = true;
     libvirtd = {
       enable = true;
+      package = pkgs.libvirt.override {
+        enableXen = false;
+        enableZfs = false;
+      };
       shutdownTimeout = 90;
       onShutdown = "shutdown";
       onBoot = "ignore";
