0bc3195caba176954e2c368691e226320e2a942e
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-04-24 21:54:49 +0200

    hardening.nix: enable tmpfs on /etc/systemd directory

--- configurations/configs/common/system-opts/hardening.nix
+++ configurations/configs/common/system-opts/hardening.nix
@@ -144,4 +144,17 @@ in
       "size=4k"
     ];
   };
+
+  ### Mount /etc/systemd as a tmpfs
+  fileSystems."/etc/systemd" = {
+    fsType = "tmpfs";
+    options = [
+      "nodev"
+      "noexec"
+      "nosuid"
+      "noswap"
+      "mode=755"
+      "size=4k"
+    ];
+  };
 }
