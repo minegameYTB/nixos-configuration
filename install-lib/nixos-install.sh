87ef552ca8bd353ff0733c42081586202a25742f
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-04-25 21:02:29 +0200

    install.sh: add post install config copy in new home directory

--- install-lib/nixos-install.sh
+++ install-lib/nixos-install.sh
@@ -252,4 +252,12 @@ nixosInstallFn() {
   getDefaultUser 6
   info "Setting password for $userName via nixos-enter"
   run_command nixos-enter --root /mnt -- passwd "$userName"
+
+  info "The configuration will be copied in your home folder"
+
+  cd .. || exit 1
+    run_command cp nixos-configuration /mnt/home/$userName
+    info "change owner of the dirctory of nixos-configuration"
+    run_command chown -R 1000:100 /mnt/home/$userName/nixos-configuration
+    #run_command chown -R --preserve-root 1000:100 /mnt/home/$userName/nixos-configuration/.*
 }
