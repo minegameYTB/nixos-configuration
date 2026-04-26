31510cb49f50906c9a13b2f473f1149fdf456e17
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-04-26 21:30:01 +0200

    system-pkgs.nix: add mdcat package and use mdless as default pager

--- configurations/configs/common/system-opts/environment.nix
+++ configurations/configs/common/system-opts/environment.nix
@@ -4,6 +4,7 @@
   ### Common env variable
   environment.variables = {
     EDITOR = "nvim";
+    PAGER = "mdless";
     NIXPKGS_ALLOW_UNFREE = "1";
   };
 
--- configurations/configs/common/system-pkgs.nix
+++ configurations/configs/common/system-pkgs.nix
@@ -16,6 +16,7 @@
       efibootmgr
       ntfs3g
       nvd
+      mdcat
 
       # Man pages
       man-pages
