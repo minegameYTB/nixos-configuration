f6827f9c2edc351d8baf3408f801082dd2de4d5f
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-05-13 18:20:31 +0200

    flake.nix: unpin kernel version (use upstream linux nixpkgs version)

--- overlay.nix
+++ overlay.nix
@@ -22,11 +22,6 @@ with inputs;
         config = nixpkgsConfig;
         overlays = [ ];
       };
-      pkgsKrnl = import nixpkgs-krnl {
-        inherit system;
-        config = nixpkgsConfig;
-        overlays = [ ];
-      };
       #pkgsLts = import ctrl-os {
       #  inherit system;
       #  config = nixpkgsConfig;
