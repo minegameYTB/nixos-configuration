34827af220d30815bea5b1e38552abfb47221c3e
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-05-17 23:02:58 +0200

    install.sh: add extra substituers to kernel

--- install-lib/nixos-install.sh
+++ install-lib/nixos-install.sh
@@ -244,7 +244,9 @@ nixosInstallFn() {
   sleep 1
   echo ""
   info "installing NixOS configuration with this profile: '$nixosProfile'"
-  run_command nixos-install --no-channel-copy --flake .#$nixosProfile
+  run_command nixos-install --no-channel-copy --flake .#$nixosProfile \
+    --option extra-substituters  "https://attic.xuyh0120.win/lantian" \
+    --option extra-trusted-public-keys "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
 
   ### Cleanup swap after installation (no-op if swap was not created)
   teardownTempSwap
