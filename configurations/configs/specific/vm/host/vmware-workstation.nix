1a930c3a9bc272343abedfffff877254f9791f81
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-05-19 00:20:30 +0200

    vmware-workstation.nix: install windows vmware tools iso

--- configurations/configs/specific/vm/host/vmware-workstation.nix
+++ configurations/configs/specific/vm/host/vmware-workstation.nix
@@ -2,5 +2,16 @@
 
 {
   ### Enable vmware workstation
-  virtualisation.vmware.host.enable = true;
+  virtualisation.vmware.host = {
+    enable = true;
+    package = pkgs.vmware-workstation.overrideAttrs (oldAttrs: {
+      installPhase = (oldAttrs.installPhase or "") + ''
+        ### Install vmware tools for windows (with .bundle file)
+        echo "Installing VMware Tools"
+        unpacked="unpacked/vmware-tools-windows"
+        cp -r $unpacked/* $out/lib/vmware/isoimages/
+      '';
+    });
+  };
+
 }
