8a98778a9013e95f441606524d295081acf7da02
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-05-19 23:23:09 +0200

    vmware-workstation.nix: add module to expose windows.iso file on vmware

--- configurations/configs/specific/vm/host/vmware-workstation.nix
+++ configurations/configs/specific/vm/host/vmware-workstation.nix
@@ -4,6 +4,7 @@
   ### Enable vmware workstation
   virtualisation.vmware.host = {
     enable = true;
+    exposeWindowsVMwareToolIso.enable = true; # Custom module (see "/configurations/modules/virtualisation/vmware-ws-iso.nix" for more info for is implementation)
     package = pkgs.vmware-workstation.overrideAttrs (oldAttrs: {
       installPhase = (oldAttrs.installPhase or "") + ''
         ### Install vmware tools for windows (with .bundle file)
--- configurations/modules/default.nix
+++ configurations/modules/default.nix
@@ -10,6 +10,7 @@
     ### Declare all modules (on all sections)
     ./programs
     ./misc
+    ./virtualisation
     #./nix
 
     ### Add other section here with a default.nix to enumerate all modules
