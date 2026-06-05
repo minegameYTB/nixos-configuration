8392a771f7ece7042c226246658214cb420d5319
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-06-05 19:40:50 +0200

    hardware-configuration/machines: precise architecture

--- configurations/hardware-configuration/machines/hp-probook/hardware-configuration.nix
+++ configurations/hardware-configuration/machines/hp-probook/hardware-configuration.nix
@@ -28,8 +28,10 @@
   ### Bluetooth
   hardware.bluetooth.enable = lib.mkForce true;
 
-  ### Root/home mount point located to ../filesystem/btrfs
+  ### Arch
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
+  ### Root/home mount point located to ../filesystem/btrfs
   ### Specific mount point
   fileSystems."/mnt/DATA" = {
     label = "DATA";
