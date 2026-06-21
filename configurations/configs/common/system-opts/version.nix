26afbc5d16c0d98ae36d9feb4415ea2572d6faf7
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-06-21 19:59:05 +0200

    version.nix: add custom os-release section

---
+++ configurations/configs/common/system-opts/version.nix
@@ -0,0 +1,18 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+
+{
+  ### custom /etc/os-release file (with system.nixos.extraOSReleaseArgs) (https://github.com/NixOS/nixpkgs/blob/28096cc5e3d8334fbe1845925f000f8c8c5e0aac/nixos/modules/misc/version.nix#L163)
+  system.nixos.extraOSReleaseArgs = rec {
+    CONFIG_URL = "https://github.com/minegameYTB/nixos-configuration";
+    #BUG_REPORT_URL = CONFIG_URL + "/issues"; # To use concatened text with a variable
+
+    ### Tests
+    #NAME = "Minegame OS";
+    #PRETTY_NAME = NAME + " " + lib.trivial.release + " " + "(${lib.trivial.codeName})";
+  };
+}
