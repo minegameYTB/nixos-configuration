33a9c89619537b23a892a30f7d9cdb68c59e92f7
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-07-03 18:23:07 +0200

    flake: add new nix expression (separate ai to other parts of the system)

---
+++ configurations/configs/specific/ai/default.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+
+{
+  ### Install specific package for AI tools (for desktop and CLI)
+  environment.systemPackages =
+    with pkgs.pkgsPr;
+    [
+      opencode
+      mcp-nixos
+    ]
+    ++ (lib.optionals config.services.xserver.enable (with pkgs.pkgsPr; [ claude-desktop ]));
+}
--- profiles/hp-probook-profile.nix
+++ profiles/hp-probook-profile.nix
@@ -30,6 +30,7 @@
     ### Other
     ../configurations/configs/specific/container/podman.nix
     ../configurations/hardware-configuration/specific/swap.nix
+    ../configurations/configs/specific/ai
 
     ### NixOS configuration module (distant flake)
     ### import default.nix from this directory ↓
