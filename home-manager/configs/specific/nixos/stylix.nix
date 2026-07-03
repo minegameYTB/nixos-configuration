1fa65df368a358480869ed85cecfcd822c2ec303
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-07-03 20:44:34 +0200

    stylix: add guard (if gnome is disabled and stylix enabled, stylix is disabled)

--- home-manager/configs/specific/nixos/stylix.nix
+++ home-manager/configs/specific/nixos/stylix.nix
@@ -1,23 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  ### stylix
-  stylix = {
-    targets = {
-      ghostty.enable = true;
-      gnome.enable = true;
-      gtksourceview.enable = true;
-      gtk = {
-        enable = true;
-        flatpakSupport.enable = true;
-      };
-      tmux.enable = false;
-      qt = {
-        ### Remove warning for qtct
-        enable = false;
-        #platform = "qtct";
-      };
+  ### Stylix targets for home-manager
+  stylix.targets = {
+    ghostty.enable = true;
+    gnome.enable = true;
+    gtksourceview.enable = true;
+    gtk = {
+      enable = true;
+      flatpakSupport.enable = true;
+    };
+    tmux.enable = false;
+    qt = {
+      ### Remove warning for qtct
+      enable = false;
+      #platform = "qtct";
     };
   };
-
 }
