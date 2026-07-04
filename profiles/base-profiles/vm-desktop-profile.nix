d26af3b776aced87ba50bad550ddd747d02a14ab
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-07-05 00:11:51 +0200

    made changes

--- profiles/base-profiles/vm-desktop-profile.nix
+++ profiles/base-profiles/vm-desktop-profile.nix
@@ -12,7 +12,6 @@
     ### X11 and desktop environment
     ../../configurations/configs/specific/desktop/environment/gnome.nix
     ../../configurations/configs/specific/desktop/sound.nix
-    ../../configurations/configs/specific/desktop/autologin.nix
     #../../configurations/configs/specific/desktop/printer.nix
     ../../configurations/configs/specific/desktop/browser
 
