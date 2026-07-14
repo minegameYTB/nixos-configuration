c57fed0a95f34c5adc1d7b144fae3b520ef2ddb0
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-07-14 15:21:49 +0200

    backport: repo URL source of truth and install script clone modes
    - lib/repo.nix: single source for repo URL
    - version.nix: use repoUrl instead of hardcoded URL
    - nixos-install.sh: 4 clone modes for step_copy_config (.git, .config-repo, lib/repo.nix, fallback)

--- configurations/configs/common/system-opts/version.nix
+++ configurations/configs/common/system-opts/version.nix
@@ -5,10 +5,13 @@
   ...
 }:
 
+let
+  repoUrl = (import ../../../../lib/repo.nix).url;
+in
 {
   ### custom /etc/os-release file (with system.nixos.extraOSReleaseArgs) (https://github.com/NixOS/nixpkgs/blob/28096cc5e3d8334fbe1845925f000f8c8c5e0aac/nixos/modules/misc/version.nix#L163)
   system.nixos.extraOSReleaseArgs = rec {
-    CONFIG_URL = "https://github.com/minegameYTB/nixos-configuration";
+    CONFIG_URL = repoUrl;
     #BUG_REPORT_URL = CONFIG_URL + "/issues"; # To use concatened text with a variable
 
     ### Tests
