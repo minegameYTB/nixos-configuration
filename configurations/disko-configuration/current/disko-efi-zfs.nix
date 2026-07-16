ddd4edd66151fea35b425a89ef3608b08f992924
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-07-16 12:25:50 +0200

    zfs: change refquota for zroot/nix/var/nix/db

--- configurations/disko-configuration/current/disko-efi-zfs.nix
+++ configurations/disko-configuration/current/disko-efi-zfs.nix
@@ -144,7 +144,7 @@
               xattr = "sa";
               dnodesize = "auto";
               recordsize = "16K";
-              refquota = "2510M"; # 2,5G
+              refquota = "8G";
             };
           };
 
