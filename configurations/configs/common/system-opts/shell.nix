34b6f656a9a8b7a3aa772ce79b7476fa63ca0986
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-07-20 18:20:08 +0200

    shell.nix: add indicator for nix develop and old nix-shell command

--- configurations/configs/common/system-opts/shell.nix
+++ configurations/configs/common/system-opts/shell.nix
@@ -11,6 +11,17 @@
     vteIntegration = true;
     interactiveShellInit = ''
       VM_OPTS="$(echo '-smp 2 -m 4096 -spice port=3001,disable-ticketing=on -device virtio-vga -display gtk')"
+
+      function nix_shell_indicator() {
+        if [[ -n $IN_NIX_SHELL ]]; then
+          RPROMPT="%{$fg_bold[cyan]%}❄️ $IN_NIX_SHELL%{$reset_color%}"
+        elif [[ -n $name ]]; then
+          RPROMPT="%{$fg_bold[cyan]%}❄️ $name%{$reset_color%}"
+        else
+          RPROMPT=""
+        fi
+      }
+      precmd_functions+=(nix_shell_indicator)
     '';
     syntaxHighlighting = {
       enable = true;
