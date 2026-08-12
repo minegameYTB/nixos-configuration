e7c184e72ba50cd7a7b90e9937fcf7518f84a49b
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-08-12 19:38:55 +0200

    opencode-sandbox: permit use of x86-64 universal micro-arch for opencode package

--- configurations/configs/specific/container/nixos-container/opencode-sandbox/container-config.nix
+++ configurations/configs/specific/container/nixos-container/opencode-sandbox/container-config.nix
@@ -15,7 +15,15 @@
   };
 
   environment.systemPackages = with pkgs.pkgsUnstable; [
-    opencode
+    (opencode.override {
+      bun = pkgs.bun.overrideAttrs (oldAttrs: rec {
+        version = "1.3.13";
+        src = pkgs.fetchurl {
+          url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64-baseline.zip";
+          hash = "sha256-nYokKSpwaAkCBdqsCloiP19pc29Sh+N7+I07QDHtx1A=";
+        };
+      });
+    })
     mcp-nixos
 
     ### Language servers
