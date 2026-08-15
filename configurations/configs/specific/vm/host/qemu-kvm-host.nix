976a63959a09cc213a805bad3ce8c51cc3a16c86
Minegame YTB <53137994+minegameYTB@users.noreply.github.com>
2026-08-15 11:46:13 +0200

    qemu-kvm-host: fix nixos testing vms settings

--- configurations/configs/specific/vm/host/qemu-kvm-host.nix
+++ configurations/configs/specific/vm/host/qemu-kvm-host.nix
@@ -43,21 +43,4 @@
       };
     };
   };
-
-  ### Nix specific
-  virtualisation.vmVariant = {
-    # following configuration is added only when building VM with build-vm
-    virtualisation = {
-      memorySize = 4096; # Use 4096MiB memory.
-      cores = 2;
-      graphics = true; # Boot the vm in a window.
-      diskSize = 15000; # Virtual machine disk size in MB.
-
-      # virgl GPU acceleration (requires OpenGL host session, e.g. X11/Wayland)
-      qemu.options = [
-        "-device virtio-vga-gl"
-        "-display gtk,gl=on"
-      ];
-    };
-  };
 }
